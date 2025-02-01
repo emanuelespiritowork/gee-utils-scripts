71fb3d367d929f3bdd4dbeb3c02893f96bac3a47
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-01 14:03:32 +0000

    Updated projects/vigne_vecchie/main_script.js (added 13 lines, deleted 1 line).

---
+++ projects/vigne_vecchie/main_script.js
@@ -0,0 +1,13 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var AOI = /* color: #d63000 */ee.Geometry.Polygon(
+        [[[8.667363735022805, 45.45548958753395],
+          [8.667798252883218, 45.45468054930045],
+          [8.668779941382668, 45.45496653623484],
+          [8.668329330268167, 45.455824488336106]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+
+
+
+var l8_coll = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+
+var l8_clipped = 
