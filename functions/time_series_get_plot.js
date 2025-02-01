5d6b766d1ed1fdc89bf32361d7523cff7dc4c751
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-01 12:00:53 +0000

    Updated functions/time_series_get_plot.js (added 1 line, deleted 1 line).

--- functions/time_series_get_plot.js
+++ functions/time_series_get_plot.js
@@ -19,7 +19,7 @@ exports.time_series_get_plot = function(time_series,which_property){
   time_series = ee.FeatureCollection(time_series);
   which_property = ee.String(which_property);
   
-  var propertyNames = ee.List([""]);
+  var propertyNames = ee.List([]);
   
   var get_property = function(feature){
     propertyNames = propertyNames.cat(feature.propertyNames()).distinct();
