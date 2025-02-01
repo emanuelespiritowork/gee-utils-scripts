fb9b64548ddfd0ed711dcc4717971e92579677ca
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-01 15:46:49 +0000

    Updated projects/vigne_vecchie/main_script.js (added 11 lines, deleted 10 lines).

--- projects/vigne_vecchie/main_script.js
+++ projects/vigne_vecchie/main_script.js
@@ -9,11 +9,12 @@ var aoi = /* color: #d63000 */ee.Feature(
           "area": "vigne",
           "system:index": "0"
         }),
-    not_aoi = /* color: #d63000 */ee.Feature(
+    geometry = /* color: #98ff00 */ee.Feature(
         ee.Geometry.Polygon(
-            [[[8.569313683898438, 45.7473038418586],
-              [8.569478821854508, 45.747012069033346],
-              [8.569768500428117, 45.74713934585177]]]),
+            [[[8.683478950314857, 44.38985101866895],
+              [8.685641226128297, 44.389347748219464],
+              [8.686666528643507, 44.390694741668156],
+              [8.684858161803355, 44.39183688780296]]]),
         {
           "area": "water",
           "system:index": "0"
@@ -43,13 +44,13 @@ var time_series_get_plot = require("users/emanuelespiritowork/SharedRepo:functio
  * street of Pernate, 28100, Novara, Italy
 *******************************************************/
 
-var AOI = ee.FeatureCollection(ee.List([aoi]));
+var AOI = ee.FeatureCollection(ee.List([aoi,geometry]));
 
 var l8_coll = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
-.filterDate("2023-01-01","2025-01-25")
-.filterBounds(aoi);
+.filterDate("2013-01-01","2025-01-25")
+.filterBounds(AOI);
 
-Map.addLayer(l8_coll);
+//Map.addLayer(l8_coll);
 
 var scale_to_use = ee.Number(30);
 
@@ -68,12 +69,12 @@ var time_series = time_series_create.time_series_create(ndfi, AOI, "area", scale
 var plot = time_series_get_plot.time_series_get_plot(time_series,"ndfi2");
 
 print(time_series);
-print(time_series.sort("ndfi2",false).first().get("ndfi2"));
+//print(time_series.sort("ndfi2",false).first().get("ndfi2"));
 print(plot);
 
 
 
-Map.addLayer(ndfi);
+//Map.addLayer(ndfi);
 
 
 
