76e70e986255f891c6642a9fa2f8db083b9e9370
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-01 14:08:02 +0000

    Updated projects/vigne_vecchie/main_script.js (added 45 lines, deleted 6 lines).

--- projects/vigne_vecchie/main_script.js
+++ projects/vigne_vecchie/main_script.js
@@ -5,9 +5,48 @@ var AOI = /* color: #d63000 */ee.Geometry.Polygon(
           [8.668779941382668, 45.45496653623484],
           [8.668329330268167, 45.455824488336106]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
 
+/******************************************************
+ * ABOUT THIS PROJECT
+ * I computed the time series of NDFI to see if any flooding happened to the Vigne Vecchie
+ * street of Pernate, 28100, Novara, Italy
+*******************************************************/
 
+/******************************************************
+ * FUNCTIONS
+*******************************************************/
+
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+var landsat_mask = require("users/emanuelespiritowork/SharedRepo:functions/landsat_mask.js");
+var landsat_scale = require("users/emanuelespiritowork/SharedRepo:functions/landsat_scale.js");
 
 var l8_coll = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+.filterDate("2013-01-01","2025-01-25");
 
-var l8_clipped = 
+var scale_to_use = ee.Number(30);
+
+var l8_clipped = clip_to.clip_to(l8_coll,AOI,scale_to_use);
+
+var l8_masked = landsat_mask.landsat_mask(l8_clipped);
+
+var l8_scaled = landsat_scale.landsat_scale(l8_masked);
+
+Map.addLayer(l8_scaled);
+
+
+
+
+
+
+
+
+
+
+
+
