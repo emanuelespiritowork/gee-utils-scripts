08a73b7f61c054cddf1be0f8ba24d4ee8e3d12cb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-01 12:04:31 +0000

    Updated functions/time_series_get_plot.js (added 1 line, deleted 1 line).

--- functions/time_series_get_plot.js
+++ functions/time_series_get_plot.js
@@ -25,7 +25,7 @@ exports.time_series_get_plot = function(time_series,which_property){
     return propertyNames.cat(feature.propertyNames()).distinct();
   };
   
-  var list_of_lists = time_series.map(get_property);
+  var list_of_lists = time_series.map(get_property).reverse().get(0);
   
   //se è mascherato non è detto che il primo lo contenga
   //bisogna ordinare prendere tutte le proprietà tra tutte le feature
