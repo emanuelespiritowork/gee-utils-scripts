5f41244185b23d33d4504e820304a2f5c6b7b838
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-02 15:41:42 +0000

    Updated functions/s2_ndwi.js (added 7 lines, deleted 1 line).

--- functions/s2_ndwi.js
+++ functions/s2_ndwi.js
@@ -17,7 +17,13 @@ exports.s2_ndwi = function(img_coll){
   img_coll = ee.ImageCollection(img_coll);
   
   var s2_ndwi_img = function(image){
-    var ndwi = image.normalizedDifference(['B3','B8']).rename('ndwi');
+    var ndwi = image.expression({
+      expression: '(GREEN - NIR) / (GREEN + NIR))',
+      map: { // Map between variables in the expression and images.
+      'NIR': image.select('B8'),
+      'GREEN': image.select('B3')
+      }
+    }).rename('ndwi');
     var time_start_value = image.get('system:time_start');
     ndwi = ndwi.set({'system:time_start':time_start_value});
     return ndwi;
