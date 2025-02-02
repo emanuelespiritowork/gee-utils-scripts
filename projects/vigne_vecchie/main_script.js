fcdc865ae835d87175857df53d44497407e4b1b8
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-02 13:53:35 +0000

    Updated projects/vigne_vecchie/main_script.js (added 81 lines, deleted 3 lines).

--- projects/vigne_vecchie/main_script.js
+++ projects/vigne_vecchie/main_script.js
@@ -81,6 +81,7 @@ print(time_series);
 //print(time_series.sort("ndvi2",false).first().get("ndvi2"));
 print(plot);
 
+/*
 Map.addLayer(l8_coll.filterDate("2014-03-28","2014-03-30")
 .filterBounds(ee.Feature(geometry).geometry()).sort("system:time_start",true).first());
 
@@ -101,11 +102,88 @@ Map.addLayer(ndvi.filterDate("2014-03-28","2014-03-30")
 
 print(ndvi.filterDate("2014-03-28","2014-03-30")
 .filterBounds(ee.Feature(geometry).geometry()));
-
+*/
 //Map.addLayer(ndvi);
 
-var image_mask = landsat_mask.landsat_mask(l8_coll.filterDate("2014-03-28","2014-03-30")
-.filterBounds(ee.Feature(geometry).geometry()));
+var landsat_mask_img = function(image){
+    var qa_pixel_layer = image.select("QA_PIXEL");
+    
+    var dilated_clouds = 1 << 1;
+    var cirrus = 1 << 2;
+    var cloud = 1 << 3;
+    var cloud_shadow = 1 << 4;
+    var snow = 1 << 5;
+    
+    var dilated_clouds_mask = qa_pixel_layer
+    .bitwiseAnd(dilated_clouds).neq(0);
+    var cirrus_mask = qa_pixel_layer
+    .bitwiseAnd(cirrus).neq(0);
+    var cloud_mask = qa_pixel_layer
+    .bitwiseAnd(cloud).neq(0);
+    var cloud_shadow_mask = qa_pixel_layer
+    .bitwiseAnd(cloud_shadow).neq(0);
+    var snow_mask = qa_pixel_layer
+    .bitwiseAnd(snow).neq(0);
+    
+    var cloud_confidence_high = ee.Image(3 << 10);
+    var cloud_confidence_medium = ee.Image(2 << 10);
+    var snow_confidence_high = ee.Image(3 << 12);
+    var snow_confidence_medium = ee.Image(2 << 12);
+    var cirrus_confidence_high = ee.Image(3 << 14);
+    var cirrus_confidence_medium = ee.Image(2 << 14);
+    
+    var select_cloud_bit = qa_pixel_layer.bitwiseAnd(cloud_confidence_high);
+    
+    var cloud_confidence_high_mask = select_cloud_bit
+    .eq(cloud_confidence_high);
+    
+    var cloud_confidence_medium_mask = select_cloud_bit
+    .eq(cloud_confidence_medium);
+    
+    var select_snow_bit = qa_pixel_layer.bitwiseAnd(snow_confidence_high);
+    
+    var snow_confidence_high_mask = select_snow_bit
+    .eq(snow_confidence_high);
+    
+    var snow_confidence_medium_mask = select_snow_bit
+    .eq(snow_confidence_medium);
+    
+    var select_cirrus_bit = qa_pixel_layer.bitwiseAnd(cirrus_confidence_high);
+    
+    var cirrus_confidence_high_mask = select_cirrus_bit
+    .eq(cirrus_confidence_high);
+    
+    var cirrus_confidence_medium_mask = select_cirrus_bit
+    .eq(cirrus_confidence_medium);
+    
+    
+    Map.addLayer(cirrus_confidence_medium_mask);
+    Map.addLayer(snow_confidence_medium_mask);
+    Map.addLayer(cloud_confidence_medium_mask);
+    //Apply masks
+    
+    var opposite_mask = ee.Image(0)
+    .or(dilated_clouds_mask)
+    .or(cirrus_mask)
+    .or(cloud_mask)
+    .or(cloud_shadow_mask)
+    .or(snow_mask)
+    .or(cloud_confidence_high_mask)
+    .or(cloud_confidence_medium_mask)
+    .or(snow_confidence_high_mask)
+    .or(snow_confidence_medium_mask)
+    .or(cirrus_confidence_high_mask)
+    .or(cirrus_confidence_medium_mask);
+    //.or(image.select("SR_B2").multiply(0.0000275).add(-0.2).gt(5000));//my mask
+    
+    //creating the xor (1 will be not cloud pixel, 0 will be cloud pixel)
+    var mask = opposite_mask.eq(0);
+    
+    return image.updateMask(mask);
+  };
+
+var image_mask = landsat_mask_img.landsat_mask_img(l8_coll.filterDate("2014-03-28","2014-03-30")
+.filterBounds(ee.Feature(geometry).geometry()).first());
 
 Map.addLayer(image_mask);
 
