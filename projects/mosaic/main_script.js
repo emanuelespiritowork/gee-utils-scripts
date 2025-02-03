b6a5dcc73e1ade6fa26e323684835d3c6f5af32c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 11:02:09 +0000

    Updated /projects/mosaic/main_script.js (added 13 lines, deleted 5 lines).

--- projects/mosaic/main_script.js
+++ projects/mosaic/main_script.js
@@ -3,8 +3,8 @@ var AOI = /* color: #d63000 */ee.FeatureCollection(
         [ee.Feature(
             ee.Geometry.Polygon(
                 [[[-73.22734375000002, 49.756348892369594],
-                  [-78.67656250000002, 47.609783081064904],
-                  [-72.87578125000002, 48.37441449527142]]]),
+                  [-74.72148437500002, 47.669003292699315],
+                  [-73.11748046875002, 47.80200338539278]]]),
             {
               "system:index": "0"
             })]);
@@ -12,7 +12,15 @@ var AOI = /* color: #d63000 */ee.FeatureCollection(
 var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
 
 var mosaic_date = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_date.js");
+/*var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
+var mosaic = mosaic_recent.mosaic_recent(s2_coll,AOI,100);
+print("mosaic",mosaic);
+*/
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 
-var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2023-12-10","2024-12-31",100);
+var clipped = clip_to.clip_to(s2_coll,AOI,100);
+print(clipped);
+//var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2023-12-10","2024-12-31",100);
 
-Map.addLayer(mosaic);
+//Map.addLayer(mosaic);
+Map.addLayer(clipped);
