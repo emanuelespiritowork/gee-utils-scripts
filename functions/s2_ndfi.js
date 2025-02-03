54122cb1b5fe89db4788f0862cc84b51a6893f94
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 09:52:33 +0000

    Updated functions/s2_ndfi.js (added 1 line, deleted 7 lines).

--- functions/s2_ndfi.js
+++ functions/s2_ndfi.js
@@ -17,13 +17,7 @@ exports.s2_ndfi = function(img_coll){
   img_coll = ee.ImageCollection(img_coll);
   
   var s2_ndfi_img = function(image){
-    var ndfi = image.expression({
-      expression: '(RED - SWIR_2)/(RED + SWIR_2)',
-      map: { // Map between variables in the expression and images.
-      'SWIR_2': image.select('B12'),
-      'RED': image.select('B4'),
-      }
-    }).rename('ndfi');
+    var ndfi = image.normalizedDifference(["B4","B12"]).rename('ndfi');
     var time_start_value = image.get('system:time_start');
     ndfi = ndfi.set({'system:time_start':time_start_value});
     return ndfi;
