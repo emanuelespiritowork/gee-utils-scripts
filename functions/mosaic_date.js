25514523a6b7236ae5f85eae9bf22bdb0f57c8dc
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 11:09:46 +0000

    Updated functions/mosaic_date.js (added 6 lines, deleted 6 lines).

--- functions/mosaic_date.js
+++ functions/mosaic_date.js
@@ -25,17 +25,17 @@ var mosaic_to = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_t
 exports.mosaic_date = function(img_coll, AOI, start_date, latest_date, scale_to_use){
   
   img_coll = ee.ImageCollection(img_coll);
-  //start_date = ee.Date(start_date);//"YYYY-MM-DD"
-  //latest_date = ee.Date(latest_date);//"YYYY-MM-DD"
-  print(start_date);
-  print(latest_date);
+  start_date = ee.Date(start_date);//"YYYY-MM-DD"
+  latest_date = ee.Date(latest_date);//"YYYY-MM-DD"
+  //print(start_date);
+  //print(latest_date);
   AOI = ee.FeatureCollection(AOI);
   scale_to_use = ee.Number(scale_to_use);
   
   var clip = clip_to.clip_to(img_coll, AOI, scale_to_use);
-  print(clip);
+  //print(clip);
   var latest_img_coll = clip.filterDate(start_date,latest_date);
-  print(latest_img_coll);
+  //print(latest_img_coll);
   var footprint = clip.first().geometry();
   
   var mosaic = mosaic_to.mosaic_to(latest_img_coll);
