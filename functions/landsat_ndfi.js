db7d41748e65128adf2e2ad3a5682295a9cd061e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 09:50:52 +0000

    Updated functions/landsat_ndfi.js (added 2 lines, deleted 7 lines).

--- functions/landsat_ndfi.js
+++ functions/landsat_ndfi.js
@@ -17,13 +17,8 @@ exports.landsat_ndfi = function(img_coll){
   img_coll = ee.ImageCollection(img_coll);
   
   var landsat_ndfi_img = function(image){
-    var ndfi = image.expression({
-      expression: '(RED - SWIR_2)/(RED + SWIR_2)',
-      map: { // Map between variables in the expression and images.
-      'SWIR_2': image.select('SR_B7'),
-      'RED': image.select('SR_B4'),
-      }
-    }).rename('ndfi');
+    
+    var ndfi = image.normalizedDifference(["SR_B4","SR_B7"]).rename('ndfi');
     
     var time_start_value = image.get('system:time_start');
     ndfi = ndfi.set({'system:time_start':time_start_value});
