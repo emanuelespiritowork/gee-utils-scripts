38061aabee48c78c684d37c4edba3c9976f84ea1
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 17:07:09 +0000

    Updated functions/s1_select.js (added 32 lines, deleted 5 lines).

--- functions/s1_select.js
+++ functions/s1_select.js
@@ -12,11 +12,38 @@
  * Description: select a desidered Sentinel-1 Ground Range Detected image
 *******************************************************/
 
-exports.s1_select = function(img_coll, acquisition, polarization, orbit){
+exports.s1_select = function(img_coll, instrument, polarization, orbit, spatial_resolution){
   
-  //refers to https://sentiwiki.copernicus.eu/web/s1-applications
+  //for choosing right parameters https://sentiwiki.copernicus.eu/web/s1-applications
+  //for explanation of parameters https://docs.sentinel-hub.com/api/latest/data/sentinel-1-grd/
   
   img_coll = ee.ImageCollection(img_coll);
+  instrument = ee.String(instrument); 
+  /*****
+   * Stripmap Mode (SM)
+   * Interferometric Wide Swath Mode (IW)
+   * Extra Wide Swath mode (EW)
+   * Wave Mode (WV) not available for Ground Range Detected.
+   *******/
+  polarization = ee.String(polarization); 
+  /*****
+   * SM: HH+HV, VV+VH, HH, VV
+   * IW: HH+HV, VV+VH, HH, VV
+   * EW: HH+HV, VV+VH, HH, VV
+   * WV:	HH, VV
+   * 
+   * It can be found an unique code for polarization:
+   * SH:	HH	
+   * SV:	VV	
+   * DH:	HH+HV	Typical for EW acquisitions
+   * DV:	VV+VH	Typical for IW acquisitions
+   * HH:	Partial Dual, HH only	HH+HV was acquired, only HH is available in this product
+   * HV:	Partial Dual, HV only	HH+HV was acquired, only HV is available in this product
+   * VV:	Partial Dual, VV only	VV+VH was acquired, only VV is available in this product
+   * VH:	Partial Dual, VH only	VV+VH was acquired, only VH is available in this product
+   *******/
+  orbit = ee.String(orbit); //DESCENDING or ASCENDING
+  spatial_resolution = ee.Number(spatial_resolution);
   
   var select_image = function(image){
     
