66d642fdefe1200266e18e00f7df8e3e8e4eb52a
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 09:23:55 +0000

    Updated functions/s2_evi.js (added 4 lines, deleted 4 lines).

--- functions/s2_evi.js
+++ functions/s2_evi.js
@@ -17,18 +17,18 @@ exports.s2_evi = function(img_coll){
   img_coll = ee.ImageCollection(img_coll);
   
   var s2_evi_img = function(image){
-    var eviExpression = image.expression({
+    var evi = image.expression({
       expression: '2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))',
       map: { // Map between variables in the expression and images.
       'NIR': image.select('B8'),
       'RED': image.select('B4'),
       'BLUE': image.select('B2')
       }
-    }).rename('evi2');
+    }).rename('evi');
     
     var time_start_value = image.get('system:time_start');
-    eviExpression = eviExpression.set({'system:time_start':time_start_value});
-    return eviExpression;
+    evi = evi.set({'system:time_start':time_start_value});
+    return evi;
   };
   
   return img_coll.map(s2_evi_img);
