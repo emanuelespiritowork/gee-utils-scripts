c436fa6b70971327063c4456b45ff245e6bb8667
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 11:10:55 +0000

    Updated /projects/mosaic/main_script.js (added 16 lines, deleted 12 lines).

--- projects/mosaic/main_script.js
+++ projects/mosaic/main_script.js
@@ -1,10 +1,13 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = /* color: #d63000 */ee.FeatureCollection(
+var AOI = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.FeatureCollection(
         [ee.Feature(
             ee.Geometry.Polygon(
-                [[[-73.22734375000002, 49.756348892369594],
-                  [-74.72148437500002, 47.669003292699315],
-                  [-73.11748046875002, 47.80200338539278]]]),
+                [[[-73.70132431186397, 49.24519193518415],
+                  [-79.12857040561397, 47.30044038568054],
+                  [-73.54751571811397, 46.55014521497358]]]),
             {
               "system:index": "0"
             })]);
@@ -12,15 +15,16 @@ var AOI = /* color: #d63000 */ee.FeatureCollection(
 var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
 
 var mosaic_date = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_date.js");
+var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
 /*var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
 var mosaic = mosaic_recent.mosaic_recent(s2_coll,AOI,100);
 print("mosaic",mosaic);
 */
-var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 
-var clipped = clip_to.clip_to(s2_coll,AOI,100);
-print(clipped);
-//var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2023-12-10","2024-12-31",100);
+//var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+//var clipped = clip_to.clip_to(s2_coll,AOI,100);
+//print(clipped);
+//Map.addLayer(clipped);
 
-//Map.addLayer(mosaic);
-Map.addLayer(clipped);
+var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2024-12-10","2024-12-31",100);
+var null_var = plot_stretch.plot_stretch(mosaic,[""],2,10);
