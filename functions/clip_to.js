13fd4b7b6a5a968482052aacbde07b213abf5319
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 11:00:17 +0000

    Updated functions/clip_to.js (added 4 lines).

--- functions/clip_to.js
+++ functions/clip_to.js
@@ -31,6 +31,10 @@ exports.clip_to = function(img_coll, AOI, scale_to_use){
     proj: projection,
     scale: area.sqrt().divide(ee.Number(10))
   }));
+  Map.addLayer(geometry.coveringGrid({
+    proj: projection,
+    scale: area.sqrt().divide(ee.Number(10))
+  }));
   print("filtered_coll",filtered_coll);
   
   var clip_img = function(image){
