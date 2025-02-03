369616d31b489f322cb594879d10e723a898023c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 14:02:46 +0000

    Updated functions/s1_speckle.js (added 3 lines, deleted 15 lines).

--- functions/s1_speckle.js
+++ functions/s1_speckle.js
@@ -14,21 +14,9 @@
 
 exports.s1_speckle = function(img_coll){
   
-  img_coll = ee.ImageCollection(img_coll);
-  
-  var s2_evi_img = function(image){
-    var evi = image.expression({
-      expression: '2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1))',
-      map: { // Map between variables in the expression and images.
-      'NIR': image.select('B8'),
-      'RED': image.select('B4'),
-      'BLUE': image.select('B2')
-      }
-    }).rename('evi');
-    var time_start_value = image.get('system:time_start');
-    evi = evi.set({'system:time_start':time_start_value});
-    return evi;
+  var speckle_lee = function(image){
+    
   };
   
-  return img_coll.map(s2_evi_img);
+  return img_coll.map(speckle_lee);
 };
