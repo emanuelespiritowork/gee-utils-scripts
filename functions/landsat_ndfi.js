fa36129adba2382dd0db61a792e5a0480cbaf898
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 09:54:18 +0000

    Updated functions/landsat_ndfi.js (deleted 2 lines).

--- functions/landsat_ndfi.js
+++ functions/landsat_ndfi.js
@@ -17,9 +17,7 @@ exports.landsat_ndfi = function(img_coll){
   img_coll = ee.ImageCollection(img_coll);
   
   var landsat_ndfi_img = function(image){
-    
     var ndfi = image.normalizedDifference(["SR_B4","SR_B7"]).rename('ndfi');
-    
     var time_start_value = image.get('system:time_start');
     ndfi = ndfi.set({'system:time_start':time_start_value});
     return ndfi;
