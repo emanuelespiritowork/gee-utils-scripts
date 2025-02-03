5899ae6a1f47ab35d39fbc26e1b99aac25b2eb15
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 14:04:50 +0000

    Updated functions/s1_select.js (added 24 lines, deleted 1 line).

---
+++ functions/s1_select.js
@@ -0,0 +1,24 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.ImageCollection or ee.Image
+ * Output: ee.ImageCollection or ee.Image
+ * Description: select a desidered Sentinel-1 Ground Range Detected image
+*******************************************************/
+
+exports.s1_select = function(img_coll){
+  
+  img_coll = ee.ImageCollection(img_coll);
+  
+  var select_image = function(image){
+    
+  };
+  
+  return img_coll.map(select_image);
+};
