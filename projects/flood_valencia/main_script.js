34066f64bf4d573c8f42e2ab8a2a15fd2b859633
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-03 18:10:03 +0000

    Updated projects/flood_valencia/main_script.js (added 1 line, deleted 1 line).

--- projects/flood_valencia/main_script.js
+++ projects/flood_valencia/main_script.js
@@ -2,6 +2,6 @@ var s1_coll = ee.ImageCollection("COPERNICUS/S1_GRD");
 
 var s1_select = require("users/emanuelespiritowork/SharedRepo:functions/s1_select.js");
 
-var selected = s1_select.s1_select(s1_coll, "IW", "VV", "DESCENDING", "H");
+var selected = s1_select.s1_select(s1_coll, "IW", "VV", "DESCENDING", "H", false);
 
 print(selected.limit(5));
