3f46794d65b900497689f4029075e7cb68e0f16a
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-04 10:03:46 +0000

    Updated functions/s1_select.js (added 1 line, deleted 1 line).

--- functions/s1_select.js
+++ functions/s1_select.js
@@ -75,7 +75,7 @@ exports.s1_select = function(img_coll, instrument, polarization, orbit, spatial_
     .filter(ee.Filter.eq("resolution",spatial_resolution))
     .select(polarization)});
   
-  print(selected_coll.limit(500));
+  print(selected_coll.first());
   
   return selected_coll;
 };
