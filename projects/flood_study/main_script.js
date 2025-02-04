95e948438558b936b84d6e81722a1d45e3e676b0
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-04 11:04:24 +0000

    Updated projects/flood_study/main_script.js (added 17 lines).

--- projects/flood_study/main_script.js
+++ projects/flood_study/main_script.js
@@ -6,6 +6,23 @@ var s1_speckle = require("users/emanuelespiritowork/SharedRepo:functions/s1_spec
 
 var selected = s1_select.s1_select(s1_coll, "IW", "ALL", "DESCENDING", "H", true);
 
+
+var dataset = ee.Image('JRC/GSW1_4/GlobalSurfaceWater');
+
+var visualization = {
+  bands: ['occurrence'],
+  min: 0.0,
+  max: 100.0,
+  palette: ['ffffff', 'ffbbbb', '0000ff']
+};
+
+Map.setCenter(59.414, 45.182, 6);
+
+Map.addLayer(dataset, visualization, 'Occurrence');
+
+
+//var bounded = selected.filterBounds
+
 //var speckled = s1_speckle.s1_speckle(selected,);
 
 print(selected.first());
