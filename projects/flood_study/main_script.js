8808fba62f013932a640e7070124e7c37d683ee0
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-04 11:38:22 +0000

    Updated projects/flood_study/main_script.js (added 43 lines, deleted 10 lines).

--- projects/flood_study/main_script.js
+++ projects/flood_study/main_script.js
@@ -1,13 +1,32 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = /* color: #d63000 */ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-93.55869988676085, 29.675437148892513],
+                  [-93.75645379301085, 33.07016901516024],
+                  [-98.96397332426085, 33.84010956990459],
+                  [-101.77647332426085, 32.237692894835845],
+                  [-96.41514519926085, 28.5042095078147]]]),
+            {
+              "system:index": "0"
+            })]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+/*****************************
+ *        FUNCTIONS
+ ***************************/
+var s1_select = require("users/emanuelespiritowork/SharedRepo:functions/s1_select.js");
+var s1_speckle = require("users/emanuelespiritowork/SharedRepo:functions/s1_speckle.js");
+var plot_map = require("users/emanuelespiritowork/SharedRepo:functions/plot_map.js");
+var mosaic_date = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_date.js");
+
+
+
+/*****************************
+ *        PROJECT
+ ***************************/
+var dataset = ee.Image('JRC/GSW1_4/GlobalSurfaceWater');
 var s1_coll = ee.ImageCollection("COPERNICUS/S1_GRD");
 
-var s1_select = require("users/emanuelespiritowork/SharedRepo:functions/s1_select.js");
-
-var s1_speckle = require("users/emanuelespiritowork/SharedRepo:functions/s1_speckle.js");
-
-var selected = s1_select.s1_select(s1_coll, "IW", "ALL", "DESCENDING", "H", true);
-
-
-var dataset = ee.Image('JRC/GSW1_4/GlobalSurfaceWater');
 
 var visualization = {
   bands: ['occurrence'],
@@ -16,13 +35,27 @@ var visualization = {
   palette: ['ffffff', 'ffbbbb', '0000ff']
 };
 
-Map.setCenter(59.414, 45.182, 6);
+//Map.setCenter(59.414, 45.182, 6);
 
 Map.addLayer(dataset, visualization, 'Occurrence');
 
 
-//var bounded = selected.filterBounds
+
+var selected = s1_select.s1_select(s1_coll, "IW", "VH", "DESCENDING", "H", false);
+
+var mosaic = mosaic_date.mosaic_date(selected,geometry,"2020-01-01","2020-12-31",300);
+
+var asc = s1_select.s1_select(s1_coll, "IW", "VH", "ASCENDING", "H", false);
+Map.addLayer(selected);
+//var null_var = plot_map.plot_map(mosaic,2,300);
+
+var mos_asc = mosaic_date.mosaic_date(asc,geometry,"2020-01-01","2020-12-31",300);
+//print(mos_asc);
+
+//var null_var_asc = plot_map.plot_map(mos_asc,2,300);
+
+//Map.addLayer(mosaic);
 
 //var speckled = s1_speckle.s1_speckle(selected,);
 
-print(selected.first());
+//print(bounded.first());
