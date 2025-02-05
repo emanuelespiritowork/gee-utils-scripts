0eab7fae6833bbd5539afaca1f7be8a9fe74984c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-05 09:22:10 +0000

    Updated projects/flood_study/main_script.js (added 2 lines, deleted 2 lines).

--- projects/flood_study/main_script.js
+++ projects/flood_study/main_script.js
@@ -47,9 +47,9 @@ var selected = s1_select.s1_select(s1_coll, "IW", "VH", "ASCENDING", "H", false)
 
 var mosaic = mosaic_date.mosaic_date(selected,geometry,"2020-01-01","2020-12-31",300);
 
-var speckle = s1_speckle.s1_speckle(mosaic,)
+var speckle = s1_speckle.s1_speckle(mosaic,30,"meters","circle");
 
-var null_var = plot_map.plot_map(mosaic,2,300);
+var null_var = plot_map.plot_map(speckle,2,300);
 
 //var speckled = s1_speckle.s1_speckle(selected,);
 
