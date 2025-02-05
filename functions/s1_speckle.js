3fc582e7a716ab2f743d30971aed5e8961715b69
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-05 09:20:06 +0000

    Updated functions/s1_speckle.js (added 7 lines, deleted 2 lines).

--- functions/s1_speckle.js
+++ functions/s1_speckle.js
@@ -14,15 +14,20 @@
  * https://www.sciencedirect.com/science/article/pii/S1569843222001911?via%3Dihub
 *******************************************************/
 
-exports.s1_speckle = function(img_coll, radius, type, meter){
+exports.s1_speckle = function(img_coll, radius, units, type){
   
   img_coll = ee.ImageCollection(img_coll);
   radius = ee.Number(radius);
+  units = ee.String(units);
   type = ee.String(type);
   //meter is a boolean
   
   var speckle_lee = function(image){
-    var remove_speckle = image.focalMean(radius,type,meter);
+    var remove_speckle = image.focalMean({
+      radius: radius,
+      kernelType: type,
+      units: units
+    });
     return remove_speckle;
   };
   
