8f0d96c2638beacb858dd733cb4a018593faff15
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-05 14:09:20 +0000

    Updated functions/histogram_map.js (added 28 lines, deleted 1 line).

---
+++ functions/histogram_map.js
@@ -0,0 +1,28 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.ImageCollection or ee.Image
+ * Output: ee.ImageCollection or ee.Image
+ * Description: create an EVI2 layer over a Landsat-8/9 L2 image collection
+*******************************************************/
+
+exports.histogram_map = function(img, AOI, scale_to_use){
+  
+  img = ee.Image(img);
+  
+  var plot = ui.Chart.image.histogram({
+    image: img,
+    region: AOI,
+    scale: scale_to_use
+  });
+  
+  print(plot);
+  
+  return 0;
+};
