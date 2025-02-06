5f4b0448b75f1af86fbdce4f95f841539cdd839c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-06 10:47:52 +0000

    Updated functions/s1_rad_terr_flatten.js (added 1 line, deleted 1 line).

--- functions/s1_rad_terr_flatten.js
+++ functions/s1_rad_terr_flatten.js
@@ -23,7 +23,7 @@ exports.s1_rad_terr_flatten = function(collection,options){
 
     // set defaults if undefined options
     options = options || {};
-    var model = options.model || 'surface';
+    var model = options.model || 'volume';
     var elevation = options.elevation || ee.Image('USGS/SRTMGL1_003');
     var buffer = options.buffer || 0;
 
