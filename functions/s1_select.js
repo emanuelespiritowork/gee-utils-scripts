4b0ee29da1a952dfff1a765dee8d010b56ab6e4d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-07 08:14:38 +0000

    Updated functions/s1_select.js (deleted 1 line).

--- functions/s1_select.js
+++ functions/s1_select.js
@@ -60,7 +60,6 @@ exports.s1_select = function(img_coll, instrument, polarization, orbit, spatial_
    *******/
    //angle is a boolean: true if you want to add angle layer to the image, 
    //false if you do not want
-   print(polarization);
    
   var selected = img_coll.filter(ee.Filter.eq("instrumentMode",instrument))
   .filter(ee.Filter.eq("orbitProperties_pass",orbit))
