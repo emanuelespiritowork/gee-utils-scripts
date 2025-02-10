ef3661ec9b7468934428b7ceb6594b917684f877
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 16:16:06 +0000

    Updated functions/s2_ndfi.js (added 5 lines, deleted 1 line).

--- functions/s2_ndfi.js
+++ functions/s2_ndfi.js
@@ -19,7 +19,11 @@ exports.s2_ndfi = function(img_coll){
   var s2_ndfi_img = function(image){
     var ndfi = image.normalizedDifference(["B4","B12"]).rename('ndfi');
     var time_start_value = image.get('system:time_start');
-    ndfi = ndfi.set({'system:time_start':time_start_value});
+    var footprint = image.get('system:footprint');
+    ndfi = ndfi.set({
+      'system:time_start':time_start_value,
+      'system:footprint': footprint
+    });
     return ndfi;
   };
   
