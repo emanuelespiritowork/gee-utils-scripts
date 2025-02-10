58f32fce6bb0f041f22e11f86d5d1658b4be281e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 14:38:54 +0000

    Updated functions/s2_scl_weights.js (added 26 lines, deleted 1 line).

--- functions/s2_scl_weights.js
+++ functions/s2_scl_weights.js
@@ -1,3 +1,28 @@
-exports.time_series_scl_weights = function(time_series, scl, AOI, scale_to_use){
+exports.s2_scl_weights = function(scl, AOI, scale_to_use){
+  
+  var set_scl_each_image = function(image){
+    
+    var set_scl_each_field = function(region){
+      
+      var scl_value = image.reduceRegion({
+        reducer: ee.Reducer.mode(),
+        geometry: region,
+        scale: scale_to_use
+      }).getNumber("SCL");
+      
+      return ee.Image(scl_value).clipToBoundsAndScale({
+        geometry: region.geometry(),
+        scale: scale_to_use
+      }).clip(region.geometry());
+    };
+    
+    var mosaic = ee.ImageCollection(AOI.map(set_scl_each_field)).mosaic();
+    
+    return mosaic;
+  };
+  
+  var scl_mode = scl.map(set_scl_each_image);
+  
+  return scl_mode;
   
 };
