284932ea517a4615b55a7fbb5dfd638b056c3f17
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 11:58:26 +0000

    Updated projects/Lomellina/nutini_script.js (added 84 lines, deleted 1 line).

---
+++ projects/Lomellina/nutini_script.js
@@ -0,0 +1,84 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/Lomellina");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// This script is for accessing satellite S2
+// created by Nutini 
+// date 15/05/2023
+// changes: update UNIMI LAB 2024/25
+ 
+//Map.addLayer(table, {color: 'yellow'});
+ 
+//var style = {color:'black', fillColor:'00000000'};
+//Map.addLayer(table2.style(style), {}, 'region', true);
+
+ 
+//Display the shapefile into the interactive map
+Map.addLayer(AOI);
+//Display the view to the center of the screen and scale the view
+Map.centerObject(AOI,20);
+//Define styling and determine the color of the shapefile 
+var styling = {color: 'black', fillColor: 'f2e1ad'};
+Map.addLayer(AOI.style(styling));
+ 
+ 
+// Filter and export  
+var RemoteCfarms = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")//get collection
+  .filterDate('2022-01-11', '2022-06-13')//period
+  .filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', 100) //cloud cover filtering
+  .filterBounds(AOI)//around AOI
+  .filterMetadata('MGRS_TILE', 'equals', '32TQQ') // force Filter by tile ID
+  .select(['B1','B2','B3','B4','B5','B6','B7','B8','B9','B11','B12'])
+  .map(function(image){return image.clip(AOI)}); //masking
+print(RemoteCfarms); //see result
+ 
+// Convert collection to list and then plot the image
+var RemoteCfarms_list = RemoteCfarms.toList(RemoteCfarms.size());
+print(RemoteCfarms_list, 'list');
+var imgS2_1 = ee.Image(RemoteCfarms_list.get(0));//0 = first element
+ 
+var bandsToDisplay = ['B8', 'B4', 'B3']; 
+Map.addLayer(imgS2_1, {bands: bandsToDisplay, min: 0, max: 3000} , 'S2 image PRE');//plot
+ 
+ 
+var stackedImageRemoteCfarms = RemoteCfarms.toBands().int16();//stack all BOA
+print(stackedImageRemoteCfarms, 'stack');
+ 
+Export.image.toDrive({
+  image: stackedImageRemoteCfarms.unmask(-9999),
+  description: 'S2_export',
+  folder: 'GEE_Export',
+  scale: 10,
+  region: AOI
+});
+ 
+ 
+ 
+//esporta tabella +++++++++++++++++++++++++++++++++++++++++++++++
+ 
+ 
+// Get band names from the first image in the collection
+var firstImage = ee.Image(RemoteCfarms.first());
+var bandNamesFirstImage = firstImage.bandNames();
+print('Band names of the first image:', bandNamesFirstImage);
+ 
+// Export band names of the first image as a table
+var bandNamesFirstImageFC = ee.FeatureCollection([ee.Feature(null, { 'BandNames': bandNamesFirstImage })]);
+Export.table.toDrive({
+  collection: bandNamesFirstImageFC,
+  description: 'bands_number',
+  folder: 'GEE_Export',
+  fileFormat: 'CSV'
+});
+ 
+// Create a table with all features from the image collection
+var allFeaturesCollection = RemoteCfarms.map(function(image) {
+  return ee.Feature(null, {'system:index': image.id()});
+});
+ 
+// Export all features from the image collection as a table
+Export.table.toDrive({
+  collection: allFeaturesCollection,
+  description: 'date_list',
+  folder: 'GEE_Export',
+  fileFormat: 'CSV'
+});
