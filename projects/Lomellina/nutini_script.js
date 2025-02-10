04421c5201feca374362a79b443a11a2f287d1bc
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 12:30:52 +0000

    Updated projects/Lomellina/nutini_script.js (added 2 lines, deleted 2 lines).

--- projects/Lomellina/nutini_script.js
+++ projects/Lomellina/nutini_script.js
@@ -29,8 +29,8 @@ var RemoteCfarms = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")//get collec
   .filterBounds(AOI)//around AOI
   //.filterMetadata('MGRS_TILE', 'equals', '32TQQ') // force Filter by tile ID
   //.select(['B1','B2','B3','B4','B5','B6','B7','B8','B9','B11','B12'])
-  .map(function(image){return image.clipToBoundsAndScale(AOI.geometry)
-  .clip(AOI.geometry)}); //masking
+  .map(function(image){return image.clipToBoundsAndScale(AOI.geometry())
+  .clip(AOI.geometry())}); //masking
 print(RemoteCfarms); //see result
  
 //var evi = s2_evi.s2_evi(RemoteCfarms);
