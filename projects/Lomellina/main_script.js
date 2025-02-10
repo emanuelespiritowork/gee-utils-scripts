d6be71998985a0546b96dc6e0d30d7d98e945e0e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 07:12:26 +0000

    Updated projects/Lomellina/main_script.js (added 4 lines, deleted 1 line).

---
+++ projects/Lomellina/main_script.js
@@ -0,0 +1,4 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/Lomellina");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
