853a514624d1f072a7513673ceb9e22d03b66843
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-10 07:17:42 +0000

    Updated projects/Lomellina/main_script.js (added 28 lines, deleted 2 lines).

--- projects/Lomellina/main_script.js
+++ projects/Lomellina/main_script.js
@@ -1,8 +1,34 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/Lomellina");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
 
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Create Lomellina time series
+*******************************************************/
+
+/******************************************************
+ * LIBRARIES
+*******************************************************/
+
+var s2_ndvi = require("users/emanuelespiritowork/SharedRepo:functions/s2_ndvi.js");
+var s2_mask = require("users/emanuelespiritowork/SharedRepo:functions/s2_mask.js");
+var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+
+/******************************************************
+ * SCRIPT
+*******************************************************/
 
 var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
 
-var ndvi = 
+var s2_mask = 
+
+var ndvi = s2_ndvi.s2_ndvi(s2_coll);
+
