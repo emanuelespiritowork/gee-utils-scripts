a2e863f4bf848f512530b79ea2421e80a803546b
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-13 14:17:34 +0000

    Updated functions/s2_scl_uniform.js (added 16 lines, deleted 1 line).

--- functions/s2_scl_uniform.js
+++ functions/s2_scl_uniform.js
@@ -1,3 +1,18 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.ImageCollection, ee.FeatureCollection
+ * Output: ee.ImageCollection
+ * Description: given an image collection with the Sentinel-2 SCL layer
+ * this script creates an SCL layer uniform in each feature
+*******************************************************/
+
 exports.s2_scl_uniform = function(img_coll, AOI){
   
   var scl = img_coll.select("SCL");
