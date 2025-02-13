b77343df6bea59832ae3cae801f3d0db703eb2e1
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-13 09:12:23 +0000

    Updated projects/mosaic/main_script.js (added 3 lines, deleted 4 lines).

--- projects/mosaic/main_script.js
+++ projects/mosaic/main_script.js
@@ -16,15 +16,14 @@ var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
 
 var mosaic_date = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_date.js");
 var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
-/*var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
+var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
 var mosaic = mosaic_recent.mosaic_recent(s2_coll,AOI,100);
-print("mosaic",mosaic);
-*/
+
 
 //var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 //var clipped = clip_to.clip_to(s2_coll,AOI,100);
 //print(clipped);
 //Map.addLayer(clipped);
 
-var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2024-12-10","2024-12-31",100);
+//var mosaic = mosaic_date.mosaic_date(s2_coll,AOI,"2024-12-10","2024-12-31",100);
 var null_var = plot_stretch.plot_stretch(mosaic,undefined,2,10);
