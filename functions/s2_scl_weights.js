13f58e9ed4f8dd3b59373ebcbc0db8205ac1a3e0
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-13 10:55:57 +0000

    Updated functions/s2_scl_weights.js (added 1 line, deleted 1 line).

--- functions/s2_scl_weights.js
+++ functions/s2_scl_weights.js
@@ -7,7 +7,7 @@ exports.s2_scl_weights = function(img_coll, AOI){
     return image.select("SCL").remap({
       from: [0,1,2,3,4,5,6,7,8,9,10,11],
       to: [0,0,0.33,0.17,1,1,1,0.33,0,0,0.33,0],
-      defaulValue: -1
+      defaultValue: -1
     }).rename("SCL_weights");
     
   };
