922bcfa2696f91a35e8ec46795cd3f846a55443d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-13 10:24:12 +0000

    Updated projects/sen2rts/main_script.js (added 2 lines, deleted 2 lines).

--- projects/sen2rts/main_script.js
+++ projects/sen2rts/main_script.js
@@ -1,5 +1,5 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/Lomellina");
+var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/Chiavenna");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 /******************************************************
  * Author: Emanuele Spirito
@@ -76,7 +76,7 @@ print(evi_scl);
 
 //var null_var = plot_map.plot_map(evi.first(),2,scale_to_use);
 
-var time_series = time_series_create.time_series_create(evi_scl,AOI,"field_name",scale_to_use);
+var time_series = time_series_create.time_series_create(evi_scl,AOI,"fid",scale_to_use);
 
 //var time_series = time_series_create.time_series_create(evi,AOI,"fid",scale_to_use);
 
