bfb3b1a1d56a1872718ab2c91d6eee13ec3d8ec2
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 08:44:42 +0000

    Updated functions/plot_class.js (added 16 lines, deleted 1 line).

---
+++ functions/plot_class.js
@@ -0,0 +1,16 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.Image
+ * Output: plot classification map
+*******************************************************/
+
+exports.plot_class = function(img){
+  
+}
