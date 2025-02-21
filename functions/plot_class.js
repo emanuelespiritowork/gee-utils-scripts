5bb43310a1ef2a88cb8103268c2dbc83e1a0fc79
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 09:05:19 +0000

    Updated functions/plot_class.js (added 30 lines, deleted 5 lines).

--- functions/plot_class.js
+++ functions/plot_class.js
@@ -11,13 +11,38 @@
  * Output: plot classification map
 *******************************************************/
 
-exports.plot_class = function(img){
+exports.plot_class = function(img, scale_to_use){
   
   img = ee.Image(img);
+  scale_to_use = ee.Number(scale_to_use);
   
-  var palette = img.randomVisualizer();
+  //see https://fluxnet.org/data/badm-data-templates/igbp-classification/
+  var igbpPalette = [
+  'aec3d4', // water
+  '111149', // wetlands
+  'cdb33b', // croplands
+  'cc0013', // urban
+  '33280d', // crop mosaic
+  'd7cdcc', // snow and ice
+  'f7e084', // barren
+  '6f6f6f',  // tundra
+  '152106', '225129', '369b47', '30eb5b', '387242', // forest
+  '6a2325', 'c3aa69', 'b76031', 'd9903d', '91af40'  // shrub, grass
+  ];
   
-  Map.addLayer(img.randomVisualizer(), {}, "classification");
+  var min = img.reduceRegion({
+    reducer: ee.Reducer.min(),
+    scale: scale_to_use,
+    bestEffort: true
+  });
+  
+  var max = img.reduceRegion({
+    reducer: ee.Reducer.max(),
+    scale: scale_to_use,
+    bestEffort: true
+  });
+  
+  Map.addLayer(, {}, "classification");
   
   return 0;
 };
