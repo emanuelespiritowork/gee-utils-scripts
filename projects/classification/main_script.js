ebee673e1158d9d9d09ad89c4c9a331840b830f4
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 08:39:54 +0000

    Updated projects/classification/main_script.js (added 1 line, deleted 1 line).

--- projects/classification/main_script.js
+++ projects/classification/main_script.js
@@ -87,7 +87,7 @@ var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
 var mosaic = mosaic_recent.mosaic_recent(s2_coll, geometry, 10);
 var null_var = plot_stretch.plot_stretch(mosaic, ["B4","B3","B2"], 2, 10);
 
-var unsup_bands = ["B8A","B12","B11"];
+var unsup_bands = ["B.*"];
 var unsup = unsup_classification.unsup_classification(mosaic.select(unsup_bands), geometry2, 6, 10);
 var null_var_2 = plot_map.plot_map(unsup, 2, 10);
 Map.addLayer(unsup, {min: 0, max: 5, palette: ["green","yellow","red","blue","black","pink"]},"unsup");
