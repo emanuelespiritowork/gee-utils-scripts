f811d31eae14f4c0cad25f78a9189db04a18709d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 08:04:12 +0000

    Updated functions/unsup_classification.js (added 17 lines, deleted 1 line).

---
+++ functions/unsup_classification.js
@@ -0,0 +1,17 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.Image, ee.Number
+ * Output: ee.Image
+ * Description: unsupervised classification of an image
+*******************************************************/
+
+exports.unsup_classification = function(img, classes){
+  
+}
