225f0b9e8cf259dbddc7e4c1356a7e7f7f90fc44
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 09:37:10 +0000

    Updated functions/landsat_ndfi.js (added 10 lines, deleted 3 lines).

--- functions/landsat_ndfi.js
+++ functions/landsat_ndfi.js
@@ -13,9 +13,16 @@
 *******************************************************/
 
 exports.landsat_ndfi = function(img_coll){
-  
+
+/******************************************************
+ * Check variable types
+ *******************************************************/
   img_coll = ee.ImageCollection(img_coll);
-  
+
+/******************************************************
+ * Create ndfi layer
+ *******************************************************/
+ 
   var landsat_ndfi_img = function(image){
     var ndfi = image.normalizedDifference(["SR_B4","SR_B7"]).rename('ndfi');
     var time_start_value = image.get('system:time_start');
