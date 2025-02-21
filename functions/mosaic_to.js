69d9e380f8473bbc674be7cb4957b1f95ccdae15
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 09:48:36 +0000

    Updated functions/mosaic_to.js (added 46 lines, deleted 22 lines).

--- functions/mosaic_to.js
+++ functions/mosaic_to.js
@@ -13,9 +13,22 @@
 *******************************************************/
 
 exports.mosaic_to = function(img_coll){
-  
+
+/******************************************************
+ * Check variable types
+ *******************************************************/
+  img_coll = ee.ImageCollection(img_coll);
+
+/******************************************************
+ * Get bands names
+ *******************************************************/
   var bands_names = img_coll.first().bandNames();
-  
+
+/******************************************************
+ * Change band order. Image collection can have images 
+ * with bands order changed through time and they cannot 
+ * be merged
+ *******************************************************/
   var change_band_order = function(img){
     var bands_types = img.bandTypes();
     var band_ordered_img = img.cast(bands_types,bands_names);
@@ -23,7 +36,12 @@ exports.mosaic_to = function(img_coll){
   };
   
   var img_coll_reordered = img_coll.map(change_band_order);
-  
+
+/******************************************************
+ * Get true latest date. Satellites are not passing every 
+ * date over each area so this function computes the true 
+ * latest date
+ *******************************************************/
   var true_latest_date = img_coll_reordered.sort({
     property: "system:time_start",
     ascending: false
@@ -32,9 +50,15 @@ exports.mosaic_to = function(img_coll){
     property: "system:time_start",
     ascending: true
   }).first().date();
-  
+
+/******************************************************
+ * Get footprint of the mosaic
+ *******************************************************/
   var footprint = img_coll.union().first().geometry();
-  
+
+/******************************************************
+ * Create mosaic
+ *******************************************************/
   var img_mosaic = img_coll_reordered.mosaic().set({
     "system:footprint": footprint,
     "system:time_start": true_start_date.millis(),
