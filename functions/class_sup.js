8e0f14a4abd261a1b7fb4c78cae4cb53f1a79519
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 09:26:09 +0000

    Updated functions/class_sup.js (added 22 lines, deleted 1 line).

---
+++ functions/class_sup.js
@@ -0,0 +1,22 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.Image, ee.FeatureCollection, ee.Number, ee.Number
+ * Output: ee.Image
+ * Description: unsupervised classification of an image
+*******************************************************/
+
+exports.class_sup = function(img, samples, scale_to_use){
+  
+  img = ee.Image(img);
+  samples = ;
+  
+  
+  
+};
