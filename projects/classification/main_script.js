3a2d32e9e08aaa6f0f611ac4bc100c57e1351d77
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-21 08:29:46 +0000

    Updated projects/classification/main_script.js (added 2 lines, deleted 1 line).

--- projects/classification/main_script.js
+++ projects/classification/main_script.js
@@ -102,3 +102,4 @@ var null_var = plot_stretch.plot_stretch(mosaic, ["B4","B3","B2"], 2, 10);
 var unsup_bands = ["B.*"];
 var unsup = unsup_classification.unsup_classification(mosaic.select(unsup_bands), geometry2, 5, 10);
 var null_var_2 = plot_map.plot_map(unsup, 2, 10);
+Map.addLayer(unsup, {palette: ["green","yellow","red","blue","black"]},"unsup");
