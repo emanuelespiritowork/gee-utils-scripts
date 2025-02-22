ec418c7f26909e1bdcbd3b660755ab544d24cbdc
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-22 14:29:45 +0000

    Updated projects/transect/main_script.js (added 21 lines, deleted 20 lines).

--- projects/transect/main_script.js
+++ projects/transect/main_script.js
@@ -1,10 +1,15 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var polygon = /* color: #d63000 */ee.Geometry.Polygon(
-        [[[8.619558853060715, 45.69066599975398],
-          [7.317554530515617, 45.30945994676962],
-          [7.427430241138058, 44.785476448834686],
-          [9.976500184830863, 45.375102432766816],
-          [9.410651067388873, 45.69833990530621]]]),
+var polygon = /* color: #d63000 */ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Polygon(
+                [[[8.619558853060715, 45.69066599975398],
+                  [7.317554530515617, 45.30945994676962],
+                  [7.427430241138058, 44.785476448834686],
+                  [9.976500184830863, 45.375102432766816],
+                  [9.410651067388873, 45.69833990530621]]]),
+            {
+              "system:index": "0"
+            })]),
     transect = /* color: #98ff00 */ee.Geometry.LineString(
         [[7.686130035103118, 45.06011833901485],
          [8.592502105415617, 45.45067304281468],
@@ -13,29 +18,25 @@ var polygon = /* color: #d63000 */ee.Geometry.Polygon(
 var bands = ['B.*'];
 
 var image_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
-/*
-var recent_mosaic_img_coll = require('users/emanuelespiritowork/SharedRepo:functions/recent_mosaic_img_coll');
 
-var img = recent_mosaic_img_coll.recent_mosaic_img_coll(image_coll,polygon,100);
-*/
-print(image_coll.first());
-print(ee.ImageCollection(image_coll.first()));
-print(ee.ImageCollection(ee.ImageCollection(image_coll.first())));
-/*
-Map.addLayer(img);
+var mosaic_recent = require('users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js');
 
-var transect_create_slice_img = require('users/emanuelespiritowork/SharedRepo:functions/transect_create_slice_img');
+//var img = mosaic_recent.mosaic_recent(image_coll,polygon,100);
+var img = ee.Image("CGIAR/SRTM90_V4")
+Map.centerObject(polygon);
 
-var slice = transect_create_slice_img.transect_create_slice_img(img.select(["B3","B4","B2"]),transect,100);
+var transect_create_slice = require('users/emanuelespiritowork/SharedRepo:functions/transect_create_slice.js');
+
+var slice = transect_create_slice.transect_create_slice(img,transect,100);
 
 print(slice);
 
-var transect_get_plot_ts = require('users/emanuelespiritowork/SharedRepo:functions/transect_get_plot_ts');
+var transect_get_plot = require('users/emanuelespiritowork/SharedRepo:functions/transect_get_plot.js');
 
-var plot = transect_get_plot_ts.transect_get_plot_ts(slice);
+var plot = transect_get_plot.transect_get_plot(slice);
 
 print(plot);
-*/
+
 //var s2_mask_img_coll = require('users/emanuelespiritowork/SharedRepo:function/s2_mask_img_coll');
 /*
 var s2_mosaic_img_coll = require('users/emanuelespiritowork/SharedRepo:functions/s2_mosaic_img_coll'); 
