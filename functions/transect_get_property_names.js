9a223b28cbd5acdba4c0951a3bce1835b85a4dc2
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-22 14:19:00 +0000

    Updated functions/transect_get_property_names.js (added 9 lines, deleted 1 line).

--- functions/transect_get_property_names.js
+++ functions/transect_get_property_names.js
@@ -9,9 +9,17 @@
  * PURPOSE OF THIS SCRIPT
  * Input: ee.FeatureCollection
  * Output: ee.List 
- * Description: find the list of property layer names of a transect
+ * Description: find the list of property layer names of a time
+ * series
 *******************************************************/
 exports.time_series_get_property_names = function(time_series){
+/******************************************************
+ * Check variable types
+*******************************************************/
+  time_series = ee.FeatureCollection(time_series);
+/******************************************************
+ * Get property names
+*******************************************************/
   var propertyNames = time_series.first().propertyNames();
   var counter = 0;
   var counter_end = time_series.size();
