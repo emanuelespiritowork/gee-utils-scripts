c38405fa62295c4a2261ec3460c3ecc715288c31
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-22 15:51:31 +0000

    Updated functions/int_find_plumes.js (added 6 lines, deleted 2 lines).

--- functions/int_find_plumes.js
+++ functions/int_find_plumes.js
@@ -1,4 +1,8 @@
-exports.int_find_plumes = function(AOI){
-  AOI = ee.FeatureCollection(AOI);
+exports.int_find_plumes = function(img_coll, scale_to_use){
+  img_coll = ee.ImageCollection(img_coll);
+  scale_to_use = ee.Number(scale_to_use);
+  
+  //a plume should be a place where I see most of the time of the year
+  //any cloud nearby 
   
 };
