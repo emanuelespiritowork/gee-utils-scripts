598b8f26cd583a30c72eb9be164f612ae50da905
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-22 14:18:05 +0000

    Updated functions/transect_get_property_names.js (added 30 lines, deleted 1 line).

---
+++ functions/transect_get_property_names.js
@@ -0,0 +1,30 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.FeatureCollection
+ * Output: ee.List 
+ * Description: find the list of property layer names of a transect
+*******************************************************/
+exports.time_series_get_property_names = function(time_series){
+  var propertyNames = time_series.first().propertyNames();
+  var counter = 0;
+  var counter_end = time_series.size();
+  while(counter <= counter_end){
+    feature = time_series.get(counter);
+    propertyNames = propertyNames.cat(feature.propertyNames()).distinct();
+  }
+  
+  var yProperties = propertyNames.remove('date')
+  .remove('system:time_start')
+  .remove('id')
+  .remove('clock')
+  .remove('system:index');
+  
+  return yProperties;
+};
