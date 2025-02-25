68f552549d6de998673d236a6303bdc3e62edeb6
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-25 10:11:20 +0000

    Updated projects/plumes/main_script.js (added 2 lines).

--- projects/plumes/main_script.js
+++ projects/plumes/main_script.js
@@ -73,6 +73,8 @@ var scored = scale.map(give_score_to_pixel);
 
 print(scored);
 
+Map.addLayer(scored.first().geometry());
+
 var clip_scored = clip_to.clip_to(scored, AOI, 10); 
 
 var null_var_2 = plot_map.plot_map(clip_scored.first(), 2, 10);
