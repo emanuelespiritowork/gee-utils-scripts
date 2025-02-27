30025695ff0956043464ad394bb9385fcd66a082
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-02-27 10:10:52 +0000

    Updated projects/count/main_script.js (added 1 line, deleted 1 line).

--- projects/count/main_script.js
+++ projects/count/main_script.js
@@ -3,7 +3,7 @@ var geometry = /* color: #d63000 */ee.Geometry.Point([14.282173659004895, 40.810
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var s1_coll = ee.ImageCollection("COPERNICUS/S1_GRD");
 
-var s1_select = require("users/emanuelespiritowork/SharedRepo:function/s1_select.js");
+var s1_select = require("users/emanuelespiritowork/SharedRepo:functions/s1_select.js");
 
 var select = s1_select.s1_select(s1_coll, "IW", "VV", "DESCENDING", "H");
 
