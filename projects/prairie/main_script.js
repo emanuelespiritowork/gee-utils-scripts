a2b70bc0c6f2d795feab16c8351d29ee6c15d826
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-04 12:20:28 +0000

    Updated projects/prairie/main_script.js (added 5 lines, deleted 2 lines).

--- projects/prairie/main_script.js
+++ projects/prairie/main_script.js
@@ -1,5 +1,8 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = /* color: #d63000 */ee.Geometry.Polygon(
+var AOI = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Polygon(
         [[[10.321288645606725, 46.159536816328036],
           [10.277000010352818, 46.09981577685726],
           [10.348754465919225, 46.0945781768611],
@@ -13,4 +16,4 @@ var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js
 var s2_clip = clip_to.clip_to(s2_coll,AOI,10);
 var dem_clip = clip_to.clip_to(dem,AOI,10);
 
-Map.addLayer(dem_clip);
+Map.addLayer(s2_coll.first());
