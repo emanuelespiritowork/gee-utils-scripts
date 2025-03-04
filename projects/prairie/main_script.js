5e4938a0155f5526d09f5a43cf96a88060ae0ed0
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-04 12:26:20 +0000

    Updated projects/prairie/main_script.js (added 2 lines, deleted 2 lines).

--- projects/prairie/main_script.js
+++ projects/prairie/main_script.js
@@ -16,11 +16,11 @@ var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosa
 var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
 
 
-var dem_clip = clip_to.clip_to(ee.ImageCollection(dem),AOI,10);
+//var dem_clip = clip_to.clip_to(ee.ImageCollection(dem),AOI,10);
 var mosaic = mosaic_recent.mosaic_recent(s2_coll,AOI,10);
 var null_var = plot_stretch.plot_stretch(mosaic,undefined,2,10);
 
-Map.addLayer(dem_clip);
+Map.addLayer(dem.clip(AOI));
 //Map.addLayer(mosaic);
 
 //a prairie has to be with small slope
