4ef9dcc0f212a5d7a45d0ca2008267a1dbe2bbbb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-04 10:21:55 +0000

    Updated functions/int_find_ships.js (added 35 lines, deleted 2 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -1,8 +1,41 @@
-exports.int_find_ships = function(img_coll, AOI, scale_to_use, threshold, connectedness){
+exports.int_find_ships = function(img_coll, AOI, scale_to_use, threshold, connectedness, radius){
   img_coll = ee.ImageCollection(img_coll);
   AOI = ee.FeatureCollection(AOI);
   threshold = ee.Number(threshold);
   scale_to_use = ee.Number(scale_to_use);
   
-  var compact = connectedness || ee.Number(10);
+  var compactness = connectedness || ee.Number(10);
+  
+  var size = radius || 3;
+  
+  var clip = clip_to.clip_to(img_coll,AOI,scale_to_use);
+  
+  var select = clip.select("VH") || clip.select("VV") || clip.select("HH") || clip.select("HV");
+  
+  var kernel_circle = ee.Kernel.circle({
+    radius: size,
+    units: "pixels",
+    normalize: false
+  });
+  
+  var get_compact_filter = function(image){
+    var start_date = image.get("system:time_start");
+    
+    var compact = image.gt(threshold)
+    .reduceNeighborhood({
+      reducer: ee.Reducer.sum(),
+      kernel: kernel_circle,
+    })
+    .gt(compactness).rename("compact")
+    .set({
+      "system:time_start": start_date
+    });
+    
+    return compact;
+  };
+  
+  var compact_filter = select.map(get_compact_filter);
+  
+  
+  
 };
