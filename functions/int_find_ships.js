15d8c26fcbaa698a34ac1469dd70d0f513f21b46
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-04 10:23:47 +0000

    Updated functions/int_find_ships.js (added 14 lines, deleted 1 line).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -36,6 +36,19 @@ exports.int_find_ships = function(img_coll, AOI, scale_to_use, threshold, connec
   
   var compact_filter = select.map(get_compact_filter);
   
-  
+  var get_max_filter = function(image){
+    var start_date = image.get("system:time_start");
+    
+    var max = image.gt(threshold)
+    .reduceNeighborhood({
+      reducer: ee.Reducer.max(),
+      kernel: kernel_circle
+    }).rename("max")
+    .set({
+      "system:time_start": start_date
+    });
+    
+    return max;
+  };
   
 };
