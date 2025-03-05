1c73f4327244742b5096aa67e65630fdcb02549d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 09:42:04 +0000

    Updated functions/int_find_prairie.js (added 1 line, deleted 3 lines).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -71,7 +71,5 @@ exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_gr
   })
   .filter(ee.Filter.gt("max",0));
   
-  var wide_vector = compact_vector.filter(ee.Filter.gt("count",wide));
-  
-  return wide_vector;
+  return compact_vector.filter(ee.Filter.gt("count",wide));
 };
