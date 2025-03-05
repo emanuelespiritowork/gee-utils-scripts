d0b8cfabddc27614818742c3fee7abb1499c4fff
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 10:17:18 +0000

    Updated functions/int_find_prairie.js (added 2 lines, deleted 2 lines).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -15,7 +15,7 @@ exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_gr
   //a prairie has to be with small slope
   
   var dem_slope = ee.Terrain.slope(dem);
-  var slope_mask = dem_slope.lt(0.5);
+  var slope_mask = dem_slope.lt(5);
   
   //a prairie has a great panorama 
   var elevation_mask = dem.gt(300);
@@ -41,7 +41,7 @@ exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_gr
   });
   
   var max_circle = ee.Kernel.circle({
-    radius: 3,
+    radius: 2,
     units: "pixels",
     normalize: false
   });
