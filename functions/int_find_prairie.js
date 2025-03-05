dc15afd5951e38727ab6aebc896f374500eb4e83
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 09:32:34 +0000

    Updated functions/int_find_prairie.js (added 7 lines, deleted 4 lines).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -1,11 +1,12 @@
 var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
-exports.int_find_prairie = function(AOI, scale_to_use, min_wide, min_height, max_slope){
+exports.int_find_prairie = function(AOI, scale_to_use, min_wide, min_height, min_grass, max_slope){
   AOI = ee.FeatureCollection(AOI);
   scale_to_use = ee.Number(scale_to_use);
   
   var wide = min_wide || ee.Number(200);
   var height = min_height || ee.Number(300);
   var slope = max_slope || ee.Number(5);
+  var grass = min_grass || ee.Number(0.2);
   
   var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
   var dem = ee.Image("CGIAR/SRTM90_V4").clip(AOI);
@@ -15,14 +16,16 @@ exports.int_find_prairie = function(AOI, scale_to_use, min_wide, min_height, max
   //a prairie has to be with small slope
   
   var dem_slope = ee.Terrain.slope(dem);
-  var slope_mask = dem_slope.lt(5);
+  var slope_mask = dem_slope.lt(slope);
   
   //a prairie has a great panorama 
-  var elevation_mask = dem.gt(300);
+  var elevation_mask = dem.gt(height);
   
   //a prairie has grass
   var ndvi = mosaic.normalizedDifference(["B8","B4"])
   .rename("ndvi");
+  
+  var grass_mask = ndvi.gt(grass);
   
 
 };
