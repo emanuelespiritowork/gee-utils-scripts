f68c6eb71ea2b6928f8239405a720b45a015a08e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 10:22:06 +0000

    Updated functions/int_find_prairie.js (added 1 line, deleted 1 line).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -82,7 +82,7 @@ exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_gr
   Map.addLayer(compact_vector);
   print(compact_vector);
   
-  var wide_vector = compact_vector.filter(ee.Filter.gt("count",compactness));
+  var wide_vector = compact_vector.filter(ee.Filter.gt("count",wide));
   
   Map.addLayer(wide_vector);
   print(wide_vector);
