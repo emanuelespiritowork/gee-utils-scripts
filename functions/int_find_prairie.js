75adba5203877a4a7277ea824f1776c6a859a783
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 10:25:14 +0000

    Updated functions/int_find_prairie.js (added 1 line, deleted 1 line).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -1,6 +1,6 @@
 var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
 exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_grass, max_slope, min_compactness){
-  AOI = ee.Geometry(AOI);
+  AOI = ee.FeatureCollection(AOI);
   
   var scale_to_use = min_scale || ee.Number(10);
   
