1efa9551898393aa673d6e3afeaf2fc8d63f0fc6
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-05 09:41:16 +0000

    Updated projects/prairie/main_script.js (added 1 line, deleted 1 line).

--- projects/prairie/main_script.js
+++ projects/prairie/main_script.js
@@ -3,7 +3,7 @@ var AOI = /* color: #d63000 */ee.Geometry.Polygon(
         [[[9.913613397802505, 46.2035982890461],
           [9.952752191747818, 45.80535134156698],
           [10.808312494482193, 46.017470710463634],
-          [11.413247186865005, 46.308522824454435]]]);
+          [11.075417597021255, 46.27625973462985]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var int_find_prairie = require("users/emanuelespiritowork/SharedRepo:functions/int_find_prairie.js");
 /*
