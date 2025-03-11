f64c6f1cae75af516b9d9daf692a2fbac7023c9e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-11 15:34:48 +0000

    Updated functions/int_find_prairie.js (deleted 2 lines).

--- functions/int_find_prairie.js
+++ functions/int_find_prairie.js
@@ -68,8 +68,6 @@ exports.int_find_prairie = function(AOI, min_scale, min_wide, min_height, min_gr
   var sort = s2_coll.filterDate(start_date.format('YYYY-MM-dd'),latest_date.format('YYYY-MM-dd'))
   .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",10));
   
-  print(sort);
-  
   var mosaic = mosaic_date.mosaic_date(sort,AOI,start_date,latest_date,scale_to_use);
   
   var ndvi = mosaic.normalizedDifference(["B8","B4"])
