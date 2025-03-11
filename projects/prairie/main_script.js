5205f49a2fdfee5b6e63feb39c03485228b919cb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-11 15:25:39 +0000

    Updated projects/prairie/main_script.js (added 1 line, deleted 1 line).

--- projects/prairie/main_script.js
+++ projects/prairie/main_script.js
@@ -18,7 +18,7 @@ var int_find_prairie = require("users/emanuelespiritowork/SharedRepo:functions/i
 Map.centerObject(AOI);
 
 var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
-var latest_date = ee.Date(s2_coll.sort({
+var latest_date = ee.Date(s2_coll.filterDate("2024-12-01").sort({
   property: "system:time_start", 
   ascending: false
 }).first().get("system:time_start"));
