6fe42a0b00e1b4d784796d8f3c6fad1854bbb496
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-12 11:15:21 +0000

    Updated functions/int_find_objects.js (added 5 lines).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -1,4 +1,9 @@
 exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
+  
+  image = ee.Image(image);
+  object_linear_dimension = ee.Number(object_linear_dimension);
+  scale_to_use = ee.Number(scale_to_use);
+  
   var seg_alg = ee.Algorithms.Image.Segmentation.SNIC({
     image: image,
     size: object_linear_dimension.divide(scale_to_use).divide(2)
