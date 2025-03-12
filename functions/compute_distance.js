f3786be882d6d8339cfba154a6ad648a8b3d596f
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-12 10:55:09 +0000

    Created functions/compute_distance.js.

---
+++ functions/compute_distance.js
@@ -0,0 +1,18 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.Geometry.Point, ee.Geometry.Point
+ * Output: ee.ImageCollection or ee.Image
+ * Description: compute distance between two points
+*******************************************************/
+
+exports.compute_distance = function(first_point,second_point){
+  
+
+};
