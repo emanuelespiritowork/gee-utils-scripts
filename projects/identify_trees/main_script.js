9b791524f131fb39429410b0fcae8136e905b5cb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 09:03:53 +0000

    Updated projects/identify_trees/main_script.js (added 5 lines, deleted 1 line).

--- projects/identify_trees/main_script.js
+++ projects/identify_trees/main_script.js
@@ -18,6 +18,10 @@ var mosaic = mosaic_recent_without_clouds.mosaic_recent_without_clouds(s2_coll,A
 
 var null_var = plot_stretch.plot_stretch(mosaic,undefined,2,10);
 
-var fields = int_find_objects.int_find_objects(mosaic,1000,10);
+//var fields = int_find_objects.int_find_objects(mosaic,1000,10);
+
+var sample = mosaic.sample();
+
+Map.addLayer(sample);
 
 Map.addLayer(fields);
