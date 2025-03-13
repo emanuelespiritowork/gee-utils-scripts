0a25734e9fc84854dc59ef49902e6294b02d7a0d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 09:01:51 +0000

    Updated functions/int_find_objects.js (added 2 lines).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -6,6 +6,8 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   
   var clusterer = ee.Clusterer.wekaXMeans(2,10);
   
+  Map.addLayer(image.geometry());
+  
   var sample = image.sample({
     region: image.geometry(),
     scale: object_linear_dimension.divide(scale_to_use).divide(2)
