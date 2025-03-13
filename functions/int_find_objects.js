301ead976ec81624104f5d17d00867776e2615ca
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 09:07:47 +0000

    Updated functions/int_find_objects.js (added 2 lines).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -9,6 +9,8 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   Map.addLayer(image.geometry());
   
   var sample = image.sample({
+    scale: object_linear_dimension.divide(scale_to_use).divide(2),
+    region: image.geometry(),
     numPixels: 100
   });
   
