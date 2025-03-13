cc06377de9ee2803bc236261340b9157eb81f3b0
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 09:47:34 +0000

    Updated functions/int_find_objects.js (added 1 line, deleted 1 line).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -35,7 +35,7 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   var seg_alg = ee.Algorithms.Image.Segmentation.SNIC({
     image: image,
     size: object_linear_dimension.divide(scale_to_use).divide(2)
-  });
+  }).select('[^seed].*');
   
   Map.addLayer(seg_alg);
   
