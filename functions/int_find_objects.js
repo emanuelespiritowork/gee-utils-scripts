1f3fc46d64d053db35ac6b51c36268cf61449ea4
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 08:37:48 +0000

    Updated functions/int_find_objects.js (added 2 lines).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -15,6 +15,8 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
     scale: object_linear_dimension.divide(scale_to_use).divide(2)
   });
   
+  Map.addLayer(clusters);
+  
   /*
   var clusterer = ee.Clusterer.wekaXMeans(2,10);
   var sample = seg_alg.sampleRegions({
