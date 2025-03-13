eb9dd8d04750750012d5141fa42ce25eafdca087
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 09:42:43 +0000

    Updated functions/int_find_objects.js (added 3 lines, deleted 9 lines).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -4,6 +4,8 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   object_linear_dimension = ee.Number(object_linear_dimension);
   scale_to_use = ee.Number(scale_to_use);
   
+  var band_name = image.bandNames();
+  
   var clusterer = ee.Clusterer.wekaXMeans(2,10);
   
   Map.addLayer(image.geometry());
@@ -37,15 +39,7 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   
   Map.addLayer(seg_alg);
   
-  var clusters_vectors = seg_alg.reduceToVectors({
-    bestEffort: true,
-    reducer: ee.Reducer.mean(),
-    scale: object_linear_dimension.divide(scale_to_use).divide(2)
-  });
-  
-  Map.addLayer(clusters_vectors);
-  
-  var classification = clusters_vectors.cluster(trained);
+  var classification = seg_alg.cluster(trained);
   
   return classification;
 };
