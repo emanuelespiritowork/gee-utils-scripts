c93e0fc7227321a1e18df9b8436cc0096051d0e5
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-03-13 08:58:42 +0000

    Updated functions/int_find_objects.js (added 1 line, deleted 1 line).

--- functions/int_find_objects.js
+++ functions/int_find_objects.js
@@ -7,7 +7,7 @@ exports.int_find_objects = function(image,object_linear_dimension,scale_to_use){
   var clusterer = ee.Clusterer.wekaXMeans(2,10);
   
   var sample = image.sample({
-    scale: scale_to_use
+    scale: object_linear_dimension.divide(scale_to_use).divide(2)
   });
   
   print(sample);
