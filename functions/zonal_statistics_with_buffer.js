641c5a78a1d209d1e736931f986a29a31ea57dcd
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-04 13:15:17 +0000

    Updated functions/zonal_statistics_with_buffer.js (added 14 lines, deleted 1 line).

--- functions/zonal_statistics_with_buffer.js
+++ functions/zonal_statistics_with_buffer.js
@@ -1,6 +1,19 @@
-exports.zonal_statistics_with_buffer = function(img, points, reducer, scale_to_use){
+var zonal_statistics = require("users/emanuelespiritowork/SharedRepo:functions/zonal_statistics.js");
+
+exports.zonal_statistics_with_buffer = function(img, points, distance, reducer, scale_to_use){
   img = ee.Image(img);
   points = ee.Geometry.MultiPoint(points);
+  distance = ee.Number(distance);
   scale_to_use = ee.Number(scale_to_use);
   
+  //create buffer
+  var create_buffer = function(point){
+    return point.buffer(distance);
+  };
+  
+  var collection_of_buffers = ee.FeatureCollection(points.map(create_buffer));
+  
+  Map.addLayer(collection_of_buffers);
+  
+  return zonal_statistics.zonal_statistics(img, collection_of_buffers, reducer, scale_to_use);
 };
