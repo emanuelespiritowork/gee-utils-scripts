f5d52006b4bd518b2addd4b4aa06f10dfe2726bb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-04 13:09:34 +0000

    Updated functions/zonal_statistics_with_buffer.js (added 3 lines).

--- functions/zonal_statistics_with_buffer.js
+++ functions/zonal_statistics_with_buffer.js
@@ -1,3 +1,6 @@
 exports.zonal_statistics_with_buffer = function(img, points, reducer, scale_to_use){
+  img = ee.Image(img);
+  points = ee.Geometry.MultiPoint(points);
+  scale_to_use = ee.Number(scale_to_use);
   
 };
