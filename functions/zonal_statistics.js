a9cb26dfc7627c02885de45baaf58563c0e17ebe
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-04 13:10:07 +0000

    Updated functions/zonal_statistics.js (added 1 line).

--- functions/zonal_statistics.js
+++ functions/zonal_statistics.js
@@ -1,6 +1,7 @@
 exports.zonal_statistics = function(img, AOI, reducer, scale_to_use){
   img = ee.Image(img);
   AOI = ee.FeatureCollection(AOI);
+  scale_to_use = ee.Number(scale_to_use);
   
   var zonal_statistics = img.reduceRegions({
     collection: AOI,
