d1b02ecb49e1c0f8f2fb838650b64422285a634a
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-19 08:23:14 +0000

    Updated functions/int_find_ships.js (added 17 lines, deleted 1 line).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -1,3 +1,19 @@
+/******************************************************
+ * REQUIRES THE FOLLOWING FUNCTIONS:
+ * clip_to
+*******************************************************/
+
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.FeatureCollection (mandatory), 
+ *        ee.Number (optional), 
+ *        ee.String (optional), 
+ *        ee.Number (optional)
+ * Output: ee.Image
+ * Description: find ships in the AOI using a image collection of the past
+*******************************************************/
 
 exports.int_find_ships = function(AOI, scale_to_use, threshold, connectedness, radius){
   
