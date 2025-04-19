194722ec0c7c5fc37f220696ba4fe02872abae97
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-19 08:56:34 +0000

    Updated functions/int_find_ships.js (added 3 lines, deleted 3 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -37,11 +37,11 @@ exports.int_find_ships = function(start_date, last_date, AOI, min_scale, min_val
   var select_IW_HV_A_H = s1_select.s1_select(s1_coll, "IW", "HV", "ASCENDING", "H");
   print(select_IW_VH_D_H.filterBounds(AOI));
   
-  var select_IW_HV_D_H_filtered = select_IW_HV_D_H.filterBounds(AOI);
+  
   print(select_IW_HV_D_H_filtered);
-  if(select_IW_HV_D_H_filtered.size() === 0)
+  if(select_IW_HV_D_H.filterBounds(AOI).size() === 0)
   {
-    select_IW_HV_D_H_filtered = null;
+    var select_IW_HV_D_H_filtered = null;
   }
   print(select_IW_VH_A_H.filterBounds(AOI));
   print(select_IW_HV_A_H.filterBounds(AOI));
