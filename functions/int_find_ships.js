f3c6754a33685a1dfa5d87b578fc9cc276ac6edb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-19 08:24:18 +0000

    Updated functions/int_find_ships.js (added 8 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -17,4 +17,12 @@ var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js
 
 exports.int_find_ships = function(AOI, scale_to_use, threshold, connectedness, radius){
   
+  AOI = ee.FeatureCollection(AOI);
+  threshold = ee.Number(threshold);
+  scale_to_use = ee.Number(scale_to_use);
+  
+  var compactness = connectedness || ee.Number(10);
+  
+  var size = radius || 3;
+  
 };
