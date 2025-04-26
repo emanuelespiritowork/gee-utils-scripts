aa9bd798eed924d144fc01bcb11b3512a1c325fb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-26 08:42:19 +0000

    Updated /functions/int_find_plumes.js (added 13 lines).

---
+++ functions/int_find_plumes.js
@@ -0,0 +1,14 @@
+
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
+//var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
+//var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
+//var plot_map = require("users/emanuelespiritowork/SharedRepo:functions/plot_map.js");
+
+
+exports.int_find_plumes = function(AOI, scale_to_use, aerosol_band, threshold){
+  var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+  .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",30));
+  
+  var clip = clip_to.clip_to(s2_coll, AOI2, 10);
+}
