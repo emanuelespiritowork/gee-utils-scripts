6b414693b6ba389a9bc664ce19602d5a2350254d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-27 10:25:08 +0000

    Updated functions/int_find_ships.js (deleted 4 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -8,14 +8,10 @@
 /******************************************************
  * REQUIRES THE FOLLOWING FUNCTIONS:
  * int_find_ships_any
- * s1_select
- * clip_to
  * s1_longest_series
 *******************************************************/
 
 var int_find_ships_any = require("users/emanuelespiritowork/SharedRepo:functions/int_find_ships_any.js");
-var s1_select = require("users/emanuelespiritowork/SharedRepo:functions/s1_select.js");
-var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 var s1_longest_series = require("users/emanuelespiritowork/SharedRepo:functions/s1_longest_series.js");
 
 /******************************************************
