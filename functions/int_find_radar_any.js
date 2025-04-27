b67cb73f4f3bbe884981b8f4f7042ae34038f736
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-27 09:50:46 +0000

    Updated functions/int_find_radar_any.js (added 6 lines, deleted 2 lines).

--- functions/int_find_radar_any.js
+++ functions/int_find_radar_any.js
@@ -1,3 +1,7 @@
-exports.int_find_radar_any = function(){
+exports.int_find_radar_any = function(img_coll, AOI, min_value, min_scale){
+  AOI = ee.FeatureCollection(AOI);
+  img_coll = ee.ImageCollection(img_coll);
   
+  var threshold = min_value || ee.Number(0);
+  var scale_to_use = min_scale || ee.Number(10);
 };
