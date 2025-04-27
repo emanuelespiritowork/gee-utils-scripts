d94157af9c6ed32452f17e0868d718e76c922529
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-27 10:03:44 +0000

    Updated functions/int_find_ships.js (added 1 line, deleted 47 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -41,53 +41,7 @@ exports.int_find_ships = function(start_date, last_date, AOI, min_scale, min_val
   var compactness = connectedness || ee.Number(10);
   var size = radius || ee.Number(3);
   
-  //create the img_coll to call the int_find_ships_any function
-  var s1_coll = ee.ImageCollection("COPERNICUS/S1_GRD")
-  .filterDate(start_date,last_date);
-  var select_IW_VH_D_H = s1_select.s1_select(s1_coll, "IW", "VH", "DESCENDING", "H");
-  var select_IW_HV_D_H = s1_select.s1_select(s1_coll, "IW", "HV", "DESCENDING", "H");
-  var select_IW_VH_A_H = s1_select.s1_select(s1_coll, "IW", "VH", "ASCENDING", "H");
-  var select_IW_HV_A_H = s1_select.s1_select(s1_coll, "IW", "HV", "ASCENDING", "H");
-  
-  /*
-  var select_IW_VH_D_H_filtered = ee.Algorithms.If({
-    condition: select_IW_VH_D_H.filterBounds(AOI).size().eq(0),
-    trueCase: undefined,
-    falseCase: select_IW_VH_D_H.filterBounds(AOI)
-  });*/
-  //print(select_IW_VH_D_H_filtered);
-  
-  
-  var select_IW_VH_D_H_filtered = select_IW_VH_D_H.filterBounds(AOI)
-  .set({'size': select_IW_VH_D_H.filterBounds(AOI).size()})
-  .select('VH');
-  
-  var select_IW_HV_D_H_filtered = select_IW_HV_D_H.filterBounds(AOI)
-  .set({'size':select_IW_HV_D_H.filterBounds(AOI).size()})
-  .select('HV');
-  
-  var select_IW_VH_A_H_filtered = select_IW_VH_A_H.filterBounds(AOI)
-  .set({'size':select_IW_VH_A_H.filterBounds(AOI).size()})
-  .select('VH');
-  
-  var select_IW_HV_A_H_filtered = select_IW_HV_A_H.filterBounds(AOI)
-  .set({'size':select_IW_HV_A_H.filterBounds(AOI).size()})
-  .select('HV');
-  
-  var list_of_image_collections = ee.List([select_IW_VH_D_H_filtered,
-  select_IW_HV_D_H_filtered,select_IW_VH_A_H_filtered,select_IW_HV_A_H_filtered]);
-  
-  var collection_of_collection_of = ee.FeatureCollection(list_of_image_collections);
-  
-  print(collection_of_collection_of);
-  
-  var collection_of_collection_of_sorted = collection_of_collection_of
-  .sort('size',false);
-  
-  var longest_image_collection = ee.ImageCollection(collection_of_collection_of_sorted
-  .first());
-  
-  print(longest_image_collection);
+  var longest_image_collection = s1_longest_series.s1_longest_series(start_date,last_date,AOI);
   
   return int_find_ships_any.int_find_ships_any(longest_image_collection,AOI,
   scale_to_use, threshold, compactness, size);
