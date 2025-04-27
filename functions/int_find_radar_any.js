a56769104d1a011b83e78942cf9cc38fbbc06f2e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-04-27 10:23:14 +0000

    Updated functions/int_find_radar_any.js (added 30 lines, deleted 3 lines).

--- functions/int_find_radar_any.js
+++ functions/int_find_radar_any.js
@@ -1,6 +1,33 @@
+/******************************************************
+ * Author: Emanuele Spirito
+ * Copyright: 2025
+ * See latest stable version on my GitHub at 
+ * https://github.com/emanuelespiritowork/gee-utils-scripts
+*******************************************************/
 
+/******************************************************
+ * REQUIRES THE FOLLOWING FUNCTIONS:
+ * int_find_ships_any
+ * s1_select
+ * clip_to
+ * s1_longest_series
+*******************************************************/
+
+var int_find_ships_any = require("users/emanuelespiritowork/SharedRepo:functions/int_find_ships_any.js");
 var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 
+/******************************************************
+ * PURPOSE OF THIS SCRIPT
+ * Input: ee.String(mandatory),
+ *        ee.String(mandatory),
+ *        ee.FeatureCollection (mandatory), 
+ *        ee.Number (optional), 
+ *        ee.Number (optional), 
+ *        ee.Number (optional),
+ *        ee.Number (optional)
+ * Output: ee.FeatureCollection
+ * Description: find ships in the AOI in a period of time
+*******************************************************/
 exports.int_find_radar_any = function(img_coll, AOI, min_value, min_scale){
   AOI = ee.FeatureCollection(AOI);
   img_coll = ee.ImageCollection(img_coll);
