5032b9b574bebdc76b94a426dd610b52c8fc7565
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-05-19 10:04:12 +0000

    Updated functions/coreg.js (added 42 lines, deleted 27 lines).

--- functions/coreg.js
+++ functions/coreg.js
@@ -19,46 +19,62 @@
 exports.coreg = function(img_ref, img_tar, band_ref, band_tar, export_folder){
   
 /******************************************************
- * Mandatory variables 
+ * Mandatory variables
 *******************************************************/
   img_ref = ee.Image(img_ref);
   img_tar = ee.Image(img_tar);
   band_ref = ee.String(band_ref);
   band_tar = ee.String(band_tar);
-  
+
+/******************************************************
+ * Optional variables
+*******************************************************/
   var folder = export_folder || ee.String("GEE_Export");
   
-  // Use bilinear resampling during registration.
+/******************************************************
+ * Resample
+*******************************************************/
+//if images are not in the same CRS one of them (the target) will be 
+//resampled into the CRS of the other (the reference). This resample is
+//by default done using nearest_neighbour but it can be chosen other 
+//resample function (bilinear o bicubic)
+
   var img_refOrig = img_ref//.resample('bilinear');
   var img_tarOrig = img_tar//.resample('bilinear');
 
-  // Choose to register using only the 'R' band.
+/******************************************************
+ * Choose the band used to coregister called R band
+*******************************************************/
   var img_refRedBand = img_refOrig.select(band_ref);
   var img_tarRedBand = img_tarOrig.select(band_tar);
 
-  // Determine the displacement by matching only the 'R' bands.
+/******************************************************
+ * Determine the displacement by matching only the 'R' bands
+*******************************************************/
   var displacement = img_tarRedBand.displacement({
     referenceImage: img_refRedBand,
     projection: img_refOrig.projection(),
-    maxOffset: 300.0,
-    patchWidth: 128.0
+    maxOffset: 300.0,//these parameters comes from trial&error bests
+    patchWidth: 128.0//these parameters comes from trial&error bests
   });
 
-  // Use the computed displacement to register all original bands.
+/******************************************************
+ * Use the computed displacement to register all original bands.
+*******************************************************/
   var registered = img_tarOrig.displace({
     displacement: displacement,
     mode: "nearest_neighbor",
     //maxOffset: 300.0
   });
   
-  //register again
+/******************************************************
+ * Register again using same parameters
+*******************************************************/
   img_tarOrig = registered;
   
-  // Choose to register using only the 'R' band.
   img_refRedBand = img_refOrig.select(band_ref);
   img_tarRedBand = img_tarOrig.select(band_tar);
   
-  // Determine the displacement by matching only the 'R' bands.
   displacement = img_tarRedBand.displacement({
     referenceImage: img_refRedBand,
     projection: img_refOrig.projection(),
@@ -66,7 +82,6 @@ exports.coreg = function(img_ref, img_tar, band_ref, band_tar, export_folder){
     patchWidth: 128.0
   });
 
-  // Use the computed displacement to register all original bands.
   registered = img_tarOrig.displace({
     displacement: displacement,
     mode: "nearest_neighbor",
