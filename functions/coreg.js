6716c6a1e774da7220eed853151121016378b00b
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-05-21 07:59:38 +0000

    Updated functions/coreg.js (added 1 line, deleted 1 line).

--- functions/coreg.js
+++ functions/coreg.js
@@ -98,7 +98,7 @@ export_folder, coreg_type){
     displacement: displacement,
     mode: ee.String(type)//"nearest_neighbor",
     //maxOffset: 300.0
-  });
+  }).reproject(img_refOrig.projection());
 
 /******************************************************
  * Register again using same parameters
