f91e7b64e68f5332b7760b553da3f2fb454f6d6c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-06-10 10:46:52 +0000

    Updated functions/s2_sigma.js (added 8 lines, deleted 1 line).

--- functions/s2_sigma.js
+++ functions/s2_sigma.js
@@ -2,5 +2,12 @@ exports.s2_sigma = function(img_coll, AOI){
   img_coll = ee.ImageCollection(img_coll);
   AOI = ee.FeatureCollection(AOI);
   
+  var kernel = ee.Kernel.circle({
+    radius: 3,
+    units: "pixels"
+  });
   
+  var compute_sigma = function(img){
+    
+  };
 };
