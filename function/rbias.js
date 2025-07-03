108392c05ff81665d460254d4849b8b73eeb9915
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-03 14:29:10 +0000

    Updated function/rbias.js (added 1 line, deleted 1 line).

--- function/rbias.js
+++ function/rbias.js
@@ -21,7 +21,7 @@ var s2 = s2coll.filterDate("2023-04-25","2023-04-27").filterBounds(AOI).first();
 var clip = s2.clip(AOI.geometry());
 var ndre = s2_ndre.s2_ndre(clip).first();
 Map.addLayer(ndre);
-var mean = ndre.reduceRegion(ee.Reducer.mean());
+var mean = ndre.reduceRegion(ee.Reducer.mean()).get("ndre");
 print(mean);
 var rbias = ndre.subtract(mean).divide(mean);
 
