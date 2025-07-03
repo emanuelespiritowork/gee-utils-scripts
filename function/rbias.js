992a881ca46391362170ef5eebccfeace9851f94
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-03 14:28:12 +0000

    Updated function/rbias.js (added 1 line, deleted 1 line).

--- function/rbias.js
+++ function/rbias.js
@@ -17,7 +17,7 @@ var s2coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED"),
 *******************************************************/
 var s2_ndre = require("users/emanuelespiritowork/SharedRepo:functions/s2_ndre.js");
 var s2 = s2coll.filterDate("2023-04-25","2023-04-27").first();
-var clip = s2.clip(AOI);
+var clip = s2.clip(AOI.geometry());
 var ndre = s2_ndre.s2_ndre(clip).first();
 Map.addLayer(ndre);
 var mean = ndre.reduceRegion(ee.Reducer.mean());
