36c233bd3614edf3fe81b18206f59e727b216a56
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-03 15:37:46 +0000

    Updated function/rbias.js (added 1 line, deleted 1 line).

--- function/rbias.js
+++ function/rbias.js
@@ -17,7 +17,7 @@ var s2coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED"),
 *******************************************************/
 var s2_ndre = require("users/emanuelespiritowork/SharedRepo:functions/s2_ndre.js");
 var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
-var id = "g";
+var id = "i";
 AOI = AOI.filter(ee.Filter.eq("id",id));
 var date = ee.Date("2025-05-17");
 var start_date = date.advance(-1,"day");
