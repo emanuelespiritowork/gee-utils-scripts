50271ea1ebdcce92af97199a4a60216131b223fb
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-09 13:40:10 +0000

    Updated projects/rbias/main_script.js (added 6 lines, deleted 1 line).

--- projects/rbias/main_script.js
+++ projects/rbias/main_script.js
@@ -1,3 +1,8 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/ESU_aggiuntive_20250703");
+var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/ESU_aggiuntive_20250703"),
+    s2coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+var s2image = s2coll.filterBounds(AOI).first();
+Map.addLayer(s2image);
+Map.addLayer(AOI);
+Map.centerObject(AOI);
