5ff8bcce8a5e76e4a41e32cd7622e6b5bc371d89
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-09 13:32:30 +0000

    Updated functions/rbias.js (added 2 lines).

--- functions/rbias.js
+++ functions/rbias.js
@@ -2,6 +2,8 @@ exports.rbias = function(image, AOI){
   
   image = ee.Image(image);//with 1 band
   
+  AOI = ee.Feature(AOI) || ee.Feature(image.geometry());
+  
   mean = image.reduce({
     reducer: ee.Reducer.mean()
   }).getNumber(0);
