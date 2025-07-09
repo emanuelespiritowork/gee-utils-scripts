6c79db564bba47d92764856a26160209b1cff74b
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-09 13:39:15 +0000

    Updated projects/rbias/main_script.js (added 3 lines, deleted 1 line).

---
+++ projects/rbias/main_script.js
@@ -0,0 +1,3 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var AOI = ee.FeatureCollection("projects/ee-emanuelespiritowork/assets/ESU_aggiuntive_20250703");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
