6c5ef83a826c082691cff6696388e653ec219004
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-13 06:50:47 +0000

    Updated projects/find_fires/main_script.js (added 3 lines).

--- projects/find_fires/main_script.js
+++ projects/find_fires/main_script.js
@@ -21,6 +21,9 @@ var data = modis.filterDate(start_date,end_date).filterBounds(AOI);
 
 //here we need a cloud mask
 
+var cloudBit = 1 << 0;
+print(cloudBit);
+
 //then we need a threshold based on:
 // absolute value
 
