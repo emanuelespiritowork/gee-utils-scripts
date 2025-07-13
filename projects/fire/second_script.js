7974d44d11fcfc018886fa97de5e12e911894458
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-13 15:11:45 +0000

    Updated projects/fire/second_script.js (added 4 lines, deleted 74 lines).

--- projects/fire/second_script.js
+++ projects/fire/second_script.js
@@ -16,7 +16,7 @@ var AOI =
             })]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var start_date = ee.Date("2019-10-23");
-var true_end_date = ee.Date("2019-11-06");
+var true_end_date = ee.Date("2019-10-29");
 
 var goes_16 = ee.ImageCollection("NOAA/GOES/16/FDCF")
 .filterDate(start_date,true_end_date)
@@ -98,79 +98,9 @@ var circleIncrease = function(end_date){
   
   //Map.addLayer(fire_outline);
   
-  return ee.Feature(fire_outline.geometry(),{date: end_date});
-  
+  //return ee.Feature(fire_outline.geometry(),{date: end_date});
+  return fire_outline.geometry();
 };
-  var goes_16 = ee.ImageCollection("NOAA/GOES/16/FDCF")
-  .filterDate(start_date,true_end_date)
-  .filterBounds(AOI); 
-
-  var goes_17 = ee.ImageCollection("NOAA/GOES/17/FDCF")
-  .filterDate(start_date,true_end_date)
-  .filterBounds(AOI);
-  
-  //var fire_mask_codes = ee.List([10,11,12,13,14,15,30,31,32,33,34,35]);
-
-//var fire_probability = ee.List([1.0,0.9,0.8,0.5,0.3,0.1,1.0,0.9,0.8,0.5,0.3,0.1]);
-
-var fire_mask_codes = [10, 30, 11, 31, 12, 32, 13, 33, 14, 34, 15, 35];
-var fire_probability = [1.0, 1.0, 0.9, 0.9, 0.8, 0.8, 0.5, 0.5, 0.3, 0.3, 0.1, 0.1];
-
-var default_value = 0;
-
-var clipping = function(image){
-  return image.clip(AOI);
-};
-
-var remap_mask_to_probability = function(image){
-  return image.remap(fire_mask_codes,
-  fire_probability,
-  default_value,
-  "Mask")
-  .select(["remapped"])
-  .rename(["Fire_probability"]);
-};
-
-//print(goes_16);
-
-var fire_from_goes_16 = goes_16.map(clipping)
-.map(remap_mask_to_probability);
-var fire_from_goes_17 = goes_17.map(clipping)
-.map(remap_mask_to_probability);
-
-var fire_footprint_from_goes_16 = fire_from_goes_16
-.reduce(ee.Reducer.max());
-var fire_footprint_from_goes_17 = fire_from_goes_17
-.reduce(ee.Reducer.max());
-
-//Map.addLayer(fire_footprint_from_goes_16,{palette:['white', 'yellow', 'orange', 'red', 'purple']},"fire_footprint_from_goes_16",false);
-//Map.addLayer(fire_footprint_from_goes_17,{palette:['white', 'yellow', 'orange', 'red', 'purple']},"fire_footprint_from_goes_17",false);
-
-//print(fire_from_goes_16);
-var linked_collection_goes = ee.ImageCollection([fire_footprint_from_goes_16,fire_footprint_from_goes_17])
-.reduce(ee.Reducer.min());
-
-//Map.addLayer(linked_collection_goes,{palette:['white', 'yellow', 'orange', 'red', 'purple']},"linked_collection_goes",false);
-
-
-var smoothed = linked_collection_goes.reduceNeighborhood({
-  reducer: ee.Reducer.mean(),
-  kernel: ee.Kernel.circle(2000,"meters",true)
-});
-
-Map.addLayer(smoothed,{palette:['white', 'yellow', 'orange', 'red', 'purple']});
-Map.addLayer(smoothed.gt(0.6))
-
-var fire_outline = smoothed.gt(ee.Image(0.6)).reduceToVectors({
-  scale: 200,
-  bestEffort: true,
-  geometry: AOI.geometry()
-}).filter(ee.Filter.eq("label",1));
-
-//Map.addLayer(fire_outline);
-
-print(fire_outline);
-
 
 var fire_outlines = datetimes.map(circleIncrease);
 
