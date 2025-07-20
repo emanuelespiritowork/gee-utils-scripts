388a901b93d53b4c622f488bf02bcdd31acdf203
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 09:09:48 +0000

    Updated /functions/int_find_fire_outline.js (added 8 lines, deleted 2 lines).

--- functions/int_find_fire_outline.js
+++ functions/int_find_fire_outline.js
@@ -1,5 +1,11 @@
 //second part: compute fire outline
 
-exports.int_find_fire_outline = function(){
+exports.int_find_fire_outline = function(fire_point, start_date, end_date, buffer){
+  fire_point = ee.Feature(fire_point);
+  start_date = ee.Date(start_date);
+  var true_end_date = ee.Date(end_date);
   
+  var buffer_value = buffer || ee.Number(10000);
+  
+  var AOI = fire_point.buffer(buffer_value);
 };
