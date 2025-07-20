cd271e5bfcedfa1cf924ee080b5d4e5de273ce2e
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 09:46:10 +0000

    Updated /functions/int_find_fire_outline.js (added 1 line).

--- functions/int_find_fire_outline.js
+++ functions/int_find_fire_outline.js
@@ -35,6 +35,7 @@ exports.int_find_fire_outline = function(fire_point, start_date, true_end_date, 
     var true_end_date_toNumber = true_end_date.millis();
     print(start_date_toNumber);
     print(true_end_date_toNumber);
+    print(delay.multiply(60).multiply(1000));
     var datetimes = ee.List.sequence(start_date_toNumber,true_end_date_toNumber,delay.multiply(60).multiply(1000))
     .map(toDate)
     .slice(1);
