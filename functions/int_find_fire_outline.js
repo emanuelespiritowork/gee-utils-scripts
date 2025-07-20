6bb14a67f0596d65690d739f00f83aaf98fe9185
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 09:23:58 +0000

    Updated /functions/int_find_fire_outline.js (added 1 line, deleted 1 line).

--- functions/int_find_fire_outline.js
+++ functions/int_find_fire_outline.js
@@ -7,7 +7,7 @@ exports.int_find_fire_outline = function(fire_point, start_date, true_end_date, 
   
   var buffer_value = buffer || ee.Number(10000);
   var delay = time_delay || ee.Number(1440);//minutes. Put 0 to get all images
-  
+  print(delay);
   var AOI = fire_point.buffer(buffer_value);
   
   Map.addLayer(AOI);
