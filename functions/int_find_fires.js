997eb201ace8ed6d9082234be7ebc083479c2f17
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 08:20:14 +0000

    Updated functions/int_find_fires.js (added 12 lines).

--- functions/int_find_fires.js
+++ functions/int_find_fires.js
@@ -1,3 +1,15 @@
 //first part: identify where fires are
 
+exports.int_find_fires = function(date, AOI, temp_threshold){
+  //mandatory 
+  date = ee.Date(date);
+  AOI = ee.FeatureCollection(AOI);
+  
+  //optional
+  var threshold = temp_threshold | ee.Number(330);
+  
+  
+  
+};
+
 //second part: compute fire outline
