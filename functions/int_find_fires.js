837daf984b0977a2509f9ca56fca021509960f98
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 08:28:18 +0000

    Updated functions/int_find_fires.js (added 7 lines, deleted 1 line).

--- functions/int_find_fires.js
+++ functions/int_find_fires.js
@@ -8,7 +8,13 @@ exports.int_find_fires = function(date, AOI, temp_threshold){
   //optional
   var threshold = temp_threshold | ee.Number(330);
   
-  
+  var modis = ee.ImageCollection("MODIS/061/MOD09CMG");
+
+  var start_date = date.advance(-1,"day");
+  var end_date = date.advance(1,"day");
+
+var data = modis.filterDate(start_date,end_date).filterBounds(AOI);
+
   
 };
 
