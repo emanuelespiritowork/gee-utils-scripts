b4146cc54c0b5bb0a682b56fab5d51d56e4c3fd2
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 08:34:57 +0000

    Updated functions/int_find_fires.js (added 25 lines, deleted 1 line).

--- functions/int_find_fires.js
+++ functions/int_find_fires.js
@@ -1,5 +1,7 @@
 //first part: identify where fires are
 
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+
 exports.int_find_fires = function(date, AOI, temp_threshold){
   //mandatory 
   date = ee.Date(date);
@@ -15,7 +17,29 @@ exports.int_find_fires = function(date, AOI, temp_threshold){
 
   var coll = modis.filterDate(start_date,end_date).filterBounds(AOI);
 
+  var clipper = clip_to.clip_to(coll,AOI,5600);
   
+  var mask_21 = function(img){
+    return img.select(["Coarse_Resolution_Brightness_Temperature_Band_21"])
+    .gt(threshold);
+  };
+  
+  var mask_31 = function(img){
+    return img.select(["Coarse_Resolution_Brightness_Temperature_Band_31"])
+    .gt(threshold);
+  };
+  
+  var masked_21 = clipper.map(mask_21)
+  .reduce(ee.Reducer.max());
+  
+  var masked_31 = clipper.map(mask_31)
+  .reduce(ee.Reducer.max());
+  
+  var masked = masked_21.or(masked_31);
+  
+  Map.addLayer(masked);
+  
+  return masked;
 };
 
 //second part: compute fire outline
