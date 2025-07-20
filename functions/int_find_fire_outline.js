8eded16f710b0f56f97d829751fb5b9247ef810a
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-07-20 10:38:25 +0000

    Updated /functions/int_find_fire_outline.js (added 1 line, deleted 1 line).

--- functions/int_find_fire_outline.js
+++ functions/int_find_fire_outline.js
@@ -37,7 +37,7 @@ exports.int_find_fire_outline = function(fire_point, start_date, true_end_date, 
     .slice(1)
   }));
   
-  print(datetimes);
+  //print(datetimes);
   
   //print(datetimes.filter(ee.Filter.gt("item",start_date.advance(,"day"))));
   var fire_mask_codes = [10, 30, 11, 31, 12, 32, 13, 33, 14, 34, 15, 35];
