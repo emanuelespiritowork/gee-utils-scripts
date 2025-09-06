af9774133bfb08668ec246e38d3e7e8bb1d2576d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-06 17:25:35 +0000

    Updated functions/int_find_flood.js (added 12 lines, deleted 9 lines).

--- functions/int_find_flood.js
+++ functions/int_find_flood.js
@@ -12,16 +12,23 @@ var get_plot_histogram = require("users/emanuelespiritowork/SharedRepo:functions
 var s1_rad_terr_flatten = require("users/emanuelespiritowork/SharedRepo:functions/s1_rad_terr_flatten.js");
 var s1_longest_series = require("users/emanuelespiritowork/SharedRepo:functions/s1_longest_series.js");
 
-exports.int_find_flood = function(start_date, last_date, AOI, min_scale, min_value){
+exports.int_find_flood = function(flood_date, AOI, min_scale, min_value){
   //mandatory inputs
-  start_date = ee.Date(start_date);//"YYYY-MM-DD"
-  last_date = ee.Date(last_date);//"YYYY-MM-DD"
+  flood_date = ee.Date(flood_date);//"YYYY-MM-DD"
   AOI = ee.FeatureCollection(AOI);
   //optional inputs
   var scale_to_use = min_scale || ee.Number(10);
   var threshold = min_value || ee.Number(-16);
   
-  var s1_series = s1_longest_series.s1_longest_series(start_date,last_date,AOI);
+  var ten_days_before_start_date = flood_date.advance(-10,"day");
+  var one_day_before_start_date = flood_date.advance(-1,"day");
+  var ten_days_after_end_date = flood_date.advance(10,"day");
+  var one_day_after_end_date = flood_date.advance(1,"day");
+  
+  var s1_series_before = s1_longest_series.s1_longest_series(ten_days_before_start_date,one_day_before_start_date,AOI);
+  var s1_series_after = s1_longest_series.s1_longest_series(ten_days_after_start_date,one_day_after_start_date,AOI);
+  
+  
   print(s1_series);
   
   var surface_water = ee.Image("JRC/GSW1_4/GlobalSurfaceWater");
@@ -34,10 +41,6 @@ exports.int_find_flood = function(start_date, last_date, AOI, min_scale, min_val
     palette: ['ffffff', 'ffbbbb', '0000ff']
   };
   
-  var ten_days_before_start_date = start_date.advance(-10,"day");
-  var one_day_before_start_date = start_date.advance(-1,"day");
-  var ten_days_after_end_date = last_date.advance(10,"day");
-  var one_day_after_end_date = last_date.advance(1,"day");
   
   var mosaic_before = mosaic_date.mosaic_date(s1_series,AOI,ten_days_before_start_date,one_day_before_start_date,scale_to_use);
   var mosaic_after = mosaic_date.mosaic_date(s1_series,AOI,one_day_after_end_date,ten_days_after_end_date,scale_to_use);
