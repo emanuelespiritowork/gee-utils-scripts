017429fd4431458db4e43d42d09f15a26f909ed2
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-06 16:56:51 +0000

    Updated functions/int_find_flood.js (added 16 lines, deleted 3 lines).

--- functions/int_find_flood.js
+++ functions/int_find_flood.js
@@ -1,3 +1,16 @@
-exports.int_find_flood = function(){
+var s1_longest_series = require("users/emanuelespiritowork/SharedRepo:functions/s1_longest_series.js");
+
+exports.int_find_flood = function(start_date, last_date, AOI, min_scale, min_value, connectedness, radius){
+  //mandatory inputs
+  start_date = ee.Date(start_date);//"YYYY-MM-DD"
+  last_date = ee.Date(last_date);//"YYYY-MM-DD"
+  AOI = ee.FeatureCollection(AOI);
+  //optional inputs
+  var scale_to_use = min_scale || ee.Number(10);
+  var threshold = min_value || ee.Number(-16);
   
-}
+  var s1_series = s1_longest_series.s1_longest_series(start_date,last_date,AOI);
+  
+  return int_find_ships_any.int_find_ships_any(s1_series,AOI,
+  scale_to_use, threshold, compactness, size);
+};
