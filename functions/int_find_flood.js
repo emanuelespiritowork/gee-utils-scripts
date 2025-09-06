f1083470a4547ecd1ecb099386860683238b8928
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-06 17:08:28 +0000

    Updated functions/int_find_flood.js (added 2 lines).

--- functions/int_find_flood.js
+++ functions/int_find_flood.js
@@ -44,6 +44,8 @@ exports.int_find_flood = function(start_date, last_date, AOI, min_scale, min_val
   var flatten_before = s1_rad_terr_flatten.s1_rad_terr_flatten(mosaic_before,scale_to_use,undefined).first();
   var flatten_after = s1_rad_terr_flatten.s1_rad_terr_flatten(mosaic_after,scale_to_use,undefined).first();
   
+  print(flatten_before);
+  
   //TO CONTINUE
 
 };
