b6766a1477031dc3292c779388f748f026170e96
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-07 13:36:34 +0000

    Updated functions/int_find_flood.js (added 1 line, deleted 1 line).

--- functions/int_find_flood.js
+++ functions/int_find_flood.js
@@ -102,7 +102,7 @@ exports.int_find_flood = function(flood_date, AOI, min_scale, min_value){
   var plain_water = connected_water.updateMask(slope_mask);
   //var null_var_4 = plot_map.plot_map(plain_water,2,10);
 
-  var flood_area = plain_water.select('[^n].*').lt(0);
+  //var flood_area = plain_water.select('[^n].*').lt(0);
   
   Map.addLayer(plain_water);
   Map.addLayer(flood_area);
