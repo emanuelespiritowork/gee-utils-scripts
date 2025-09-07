78b9fc0d5100f764e180eaa49b22527459e89666
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-07 13:53:29 +0000

    Updated projects/plumes/main_script.js (added 5 lines, deleted 5 lines).

--- projects/plumes/main_script.js
+++ projects/plumes/main_script.js
@@ -41,12 +41,11 @@ var AOI =
 var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
 .filter(ee.Filter.lt("CLOUDY_PIXEL_PERCENTAGE",30));
 Map.centerObject(AOI2);
-
+/*
 var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
 var mosaic_recent = require("users/emanuelespiritowork/SharedRepo:functions/mosaic_recent.js");
 var plot_stretch = require("users/emanuelespiritowork/SharedRepo:functions/plot_stretch.js");
-var plot_map = require("users/emanuelespiritowork/SharedRepo:functions/plot_map.js");
 
 var clip = clip_to.clip_to(s2_coll, AOI2, 10);
 
@@ -57,10 +56,11 @@ var scale = s2_scale.s2_scale(clip);
 //var null_var = plot_stretch.plot_stretch(mosaic, ["B4","B3","B2"], 2, 10);
 var null_var = plot_stretch.plot_stretch(clip.first(), ["B4","B3","B2"], 2, 10);
 //var null_var_2 = plot_map.plot_map(scale.first().select("B1"),2,10);
-
+*/
 
 var int_find_plumes_any = require("users/emanuelespiritowork/SharedRepo:functions/int_find_plumes_any.js");
 var int_find_plumes = require("users/emanuelespiritowork/SharedRepo:functions/int_find_plumes.js");
+var plot_map = require("users/emanuelespiritowork/SharedRepo:functions/plot_map.js");
 
 //var score = int_find_plumes_any.int_find_plumes_any(scale, AOI2, 10, 0.2, "B1");
 
