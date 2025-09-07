714d9673b74b31b43e8eb3ca082d1eabdd7260e3
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-07 13:04:22 +0000

    Updated functions/int_find_radar_any.js (added 7 lines, deleted 1 line).

--- functions/int_find_radar_any.js
+++ functions/int_find_radar_any.js
@@ -34,7 +34,13 @@ exports.int_find_radar_any = function(img_coll, AOI, min_value, min_scale){
   .reduce(ee.Reducer.max());
   
   var radar_location = max_value_pixel.gt(threshold)
-  .reduceToVectors();
+  .reduceToVectors({
+      scale: scale_to_use,
+      bestEffort: true,
+      reducer: ee.Reducer.max()
+    })
+    .filter(ee.Filter.gt("label",0))
+    .filter(ee.Filter.gt("max",0));
   
   return clip_to.clip_to(radar_location,AOI,scale_to_use);
 };
