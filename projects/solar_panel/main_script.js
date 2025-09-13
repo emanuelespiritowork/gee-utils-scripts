a2b91f82a50335f194638ac8e1bc58c874d2b14d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 07:57:56 +0000

    Updated projects/solar_panel/main_script.js (added 10 lines, deleted 6 lines).

--- projects/solar_panel/main_script.js
+++ projects/solar_panel/main_script.js
@@ -1,12 +1,16 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = /* color: #d63000 */ee.Geometry.Polygon(
-        [[[9.664783858022652, 45.586929029926026],
-          [9.636996172628365, 45.58527717482284],
-          [9.63635244246479, 45.57637133678865],
-          [9.676349543294869, 45.5772724976208]]]);
+var AOI = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Polygon(
+        [[[12.435676696144608, 44.150596682692665],
+          [12.42812359555867, 44.152259440868036],
+          [12.421085479103592, 44.145977664188536],
+          [12.43087017758992, 44.14135828422289],
+          [12.445890548073319, 44.14382199851967]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var int_find_solar_panel = require("users/emanuelespiritowork/SharedRepo:functions/int_find_solar_panel.js");
 
-var image = int_find_solar_panel.int_find_solar_panel(AOI,"2016-08-01","2025-02-04");
+var image = int_find_solar_panel.int_find_solar_panel(AOI,"2024-08-01","2025-02-04");
 
 Map.addLayer(image);
