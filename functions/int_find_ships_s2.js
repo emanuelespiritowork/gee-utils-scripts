228eb4efa357e6bf7a07d349ca39097a03c1c325
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 09:47:11 +0000

    Updated functions/int_find_ships_s2.js (added 3 lines, deleted 3 lines).

--- functions/int_find_ships_s2.js
+++ functions/int_find_ships_s2.js
@@ -1,10 +1,10 @@
-exports.int_find_ships_s2 = function(img_coll,AOI,radius,iterations,min_scale){
+exports.int_find_ships_s2 = function(img_coll,AOI,radius,n_iterations,min_scale){
   AOI = ee.FeatureCollection(AOI);
   radius = ee.Number(radius);
-  iterations = ee.Number(iterations);
+  var iterations = iterations || ee.Number(1);
   var scale_to_use = min_scale || ee.Number(10);
   
-  var clip = clip_to.clip_to(img_coll,AOI,scale_to_use)
+  var clip = clip_to.clip_to(img_coll,AOI,scale_to_use);
   
 var mask_1 = img_coll.first().normalizedDifference(["B8","B4"]).lt(-0.15);
 var mask_2 = imageCollection.filterBounds(geometry).first().normalizedDifference(["B8","B4"]).gt(-0.4);
