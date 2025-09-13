248ffd5f6e0461e1bb8889d6a55cb6bdf634cb76
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 07:39:04 +0000

    Updated functions/int_find_solar_panel.js (added 5 lines, deleted 1 line).

--- functions/int_find_solar_panel.js
+++ functions/int_find_solar_panel.js
@@ -1,3 +1,7 @@
+var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
+
+
 exports.int_find_solar_panel = function(AOI, start_date, end_date, min_scale){
   AOI = ee.FeatureCollection(AOI);
   start_date = ee.Date(start_date);
