69ce90cb00a36586acaec90a6f509dd240d5ac9c
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 09:28:31 +0000

    Updated functions/int_find_ships.js (added 22 lines, deleted 5 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -27,20 +27,37 @@ var s1_longest_series = require("users/emanuelespiritowork/SharedRepo:functions/
  * Description: find ships in the AOI in a period of time
 *******************************************************/
 
-exports.int_find_ships = function(start_date, last_date, AOI, min_scale, min_value, max_value, connectedness, radius){
+exports.int_find_ships = function(start_date, last_date, AOI, min_scale, s1_min_value, s1_max_value, s2_min_value, s2_max_value, connectedness, radius){
   //mandatory inputs
   start_date = ee.Date(start_date);//"YYYY-MM-DD"
   last_date = ee.Date(last_date);//"YYYY-MM-DD"
   AOI = ee.FeatureCollection(AOI);
   //optional inputs
   var scale_to_use = min_scale || ee.Number(10);
-  var low_threshold = min_value || ee.Number(-16);
-  var up_threshold = max_value || ee.Number(0);
+  var s1_low_threshold = s1_min_value || ee.Number(-16);
+  var s1_up_threshold = s1_max_value || ee.Number(0);
+  var s2_low_threshold = s2_min_value || ee.Number(-0.4);
+  var s2_up_threshold = s2_max_value || ee.Number(-0.15);
   var compactness = connectedness || ee.Number(10);
   var size = radius || ee.Number(3);
   
   var s1_series = s1_longest_series.s1_longest_series(start_date,last_date,AOI);
   
-  return int_find_ships_any.int_find_ships_any(s1_series,AOI,
-  scale_to_use, low_threshold, up_threshold, compactness, size);
+  var s1_ships = int_find_ships_any.int_find_ships_any(s1_series,AOI,
+  scale_to_use, s1_low_threshold, s1_up_threshold, compactness, size);
+  
+  var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+  .filterBounds(AOI)
+  .filterDate(start_date,last_date);
+  
+  var clip = clip_to.clip_to(s2_coll,AOI,scale_to_use);
+  
+  var scale = s2_scale.s2_scale(clip);
+  
+  var s2_series = s2_ndvi.s2_ndvi(scale);
+  
+  var s2_ships = int_find_ships_any.int_find_ships_any(s2_series,AOI,
+  scale_to_use, s2_low_threshold, s2_up_threshold, compactness, size);
+  
+  return(s1_ships);
 };
