e3547e6af585c750d62066502d142641bba42e68
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 09:19:59 +0000

    Updated functions/int_find_ships.js (added 4 lines, deleted 3 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -27,19 +27,20 @@ var s1_longest_series = require("users/emanuelespiritowork/SharedRepo:functions/
  * Description: find ships in the AOI in a period of time
 *******************************************************/
 
-exports.int_find_ships = function(start_date, last_date, AOI, min_scale, min_value, connectedness, radius){
+exports.int_find_ships = function(start_date, last_date, AOI, min_scale, min_value, max_value, connectedness, radius){
   //mandatory inputs
   start_date = ee.Date(start_date);//"YYYY-MM-DD"
   last_date = ee.Date(last_date);//"YYYY-MM-DD"
   AOI = ee.FeatureCollection(AOI);
   //optional inputs
   var scale_to_use = min_scale || ee.Number(10);
-  var threshold = min_value || ee.Number(-16);
+  var low_threshold = min_value || ee.Number(-16);
+  var up_threshold = max_value || ee.Number(0);
   var compactness = connectedness || ee.Number(10);
   var size = radius || ee.Number(3);
   
   var s1_series = s1_longest_series.s1_longest_series(start_date,last_date,AOI);
   
   return int_find_ships_any.int_find_ships_any(s1_series,AOI,
-  scale_to_use, threshold, compactness, size);
+  scale_to_use, low_threshold, up_threshold, compactness, size);
 };
