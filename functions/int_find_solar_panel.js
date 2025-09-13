20e7a3e6fd82368c51238bf97475a49a2200fa4d
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-13 07:35:49 +0000

    Updated functions/int_find_solar_panel.js (added 23 lines, deleted 3 lines).

--- functions/int_find_solar_panel.js
+++ functions/int_find_solar_panel.js
@@ -1,3 +1,23 @@
-exports.int_find_solar_panel = function(AOI, start_date, end_date){
+exports.int_find_solar_panel = function(AOI, start_date, end_date, min_scale){
+  AOI = ee.FeatureCollection(AOI);
+  start_date = ee.Date(start_date);
+  end_date = ee.Date(end_date);
   
-}
+  var scale_to_use = min_scale || ee.Number(10);
+  
+  var s2_coll = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+  .filterBounds(AOI)
+  .filterDate(start_date,end_date);
+  
+  var clip = clip_to.clip_to(s2_coll,AOI,scale_to_use);
+  
+  var scale = s2_scale.s2_scale(clip);
+  
+  var max = scale.reduce({
+    reducer: ee.Reducer.max()
+  });
+  
+  var mask = max.gt(1);
+  
+  return(mask);
+};
