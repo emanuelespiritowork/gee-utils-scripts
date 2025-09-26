f4de04b8affe5bfebc75d0d2c91bd079ab6904ab
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-09-26 06:12:32 +0000

    Updated functions/int_find_ships.js (added 2 lines, deleted 2 lines).

--- functions/int_find_ships.js
+++ functions/int_find_ships.js
@@ -51,8 +51,8 @@ exports.int_find_ships = function(start_date, last_date, AOI, min_scale, s1_min_
   Map.addLayer(s1_series.first());
   print(s1_series.first());
   
-  Map.addLayer(s1_series.sort("start_date").first());
-  print(s1_series.sort("start_date",false).first());
+  Map.addLayer(s1_series.sort("system:time_start").first());
+  print(s1_series.sort("system:time_start",false).first());
   
   var s1_ships = int_find_ships_any.int_find_ships_any(s1_series,AOI,
   scale_to_use, s1_low_threshold, s1_up_threshold, compactness, size);
