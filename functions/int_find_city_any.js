6c783ddfdab0459e6ef4c9725c326e55e5f50cf3
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:12:45 +0000

    Updated functions/int_find_city_any.js (added 3 lines, deleted 4 lines).

--- functions/int_find_city_any.js
+++ functions/int_find_city_any.js
@@ -1,13 +1,12 @@
 var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
 
-exports.int_find_city = function(start_date, end_date, AOI, min_scale, min_value){
+exports.int_find_city_any = function(img_coll, start_date, end_date, AOI, scale_to_use, threshold){
   //mandatory inputs
   start_date = ee.Date(start_date);//"YYYY-MM-DD"
   end_date = ee.Date(end_date);//"YYYY-MM-DD"
   AOI = ee.FeatureCollection(AOI);
-  //optional inputs
-  var scale_to_use = ee.Number(min_scale || 10);
-  var threshold = ee.Number(min_value || 40);
+  scale_to_use = ee.Number(scale_to_use);
+  threshold = ee.Number(threshold);
   
   var result = int_find_city_any.int_find_city_any({
     img_coll: ee.ImageCollection("NASA/VIIRS/002/VNP46A2"),
