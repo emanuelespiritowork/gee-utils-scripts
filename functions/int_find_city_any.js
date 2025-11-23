138fa4209a3f3c5e5f700c21474981b8bc9fd029
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:13:49 +0000

    Updated functions/int_find_city_any.js (added 20 lines, deleted 8 lines).

--- functions/int_find_city_any.js
+++ functions/int_find_city_any.js
@@ -8,14 +8,26 @@ exports.int_find_city_any = function(img_coll, start_date, end_date, AOI, scale_
   scale_to_use = ee.Number(scale_to_use);
   threshold = ee.Number(threshold);
   
-  var result = int_find_city_any.int_find_city_any({
-    img_coll: ee.ImageCollection("NASA/VIIRS/002/VNP46A2"),
-    start_date: start_date,
-    end_date: end_date,
-    AOI: AOI,
-    scale_to_use: scale_to_use,
-    threshold: threshold 
-  });
+  var filter = img_coll.filterDate(start_date, end_date);
+  
+  var clip = clip_to.clip_to(filter,AOI,10).first();
+
+var vect = ee.Image(clip).select(["Gap_Filled_DNB_BRDF_Corrected_NTL"])
+.gt(ee.Image(40))
+.reduceToVectors({
+  bestEffort: true
+})
+.filter(ee.Filter.eq("label",1));
+
+Map.addLayer(clip, {
+  bands: "Gap_Filled_DNB_BRDF_Corrected_NTL",
+  min:10,
+  max:80
+});
+
+Map.addLayer(vect);
+
+Map.centerObject(AOI);
   
   return(result);
 };
