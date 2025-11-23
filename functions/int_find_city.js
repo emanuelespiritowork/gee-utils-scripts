d929a5b94923daa0f396e9fc6c55028bd3c637d6
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:44:41 +0000

    Updated functions/int_find_city.js (added 1 line, deleted 1 line).

--- functions/int_find_city.js
+++ functions/int_find_city.js
@@ -6,7 +6,7 @@ exports.int_find_city = function(start_date, end_date, AOI, min_scale, min_value
   end_date = ee.Date(end_date);//"YYYY-MM-DD"
   AOI = ee.FeatureCollection(AOI);
   //optional inputs
-  var scale_to_use = ee.Number(min_scale || 10);
+  var scale_to_use = ee.Number(min_scale || 500);
   var threshold = ee.Number(min_value || 40);
 
   var result = int_find_city_any.int_find_city_any(ee.ImageCollection("NASA/VIIRS/002/VNP46A2"),
