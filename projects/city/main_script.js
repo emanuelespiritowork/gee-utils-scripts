bd7a5908ea7dcec9c6b3fd5c3feac16867eafe84
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:45:40 +0000

    Updated projects/city/main_script.js (added 5 lines, deleted 5 lines).

--- projects/city/main_script.js
+++ projects/city/main_script.js
@@ -1,5 +1,8 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var AOI = /* color: #d63000 */ee.FeatureCollection(
+var AOI = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.FeatureCollection(
         [ee.Feature(
             ee.Geometry.Polygon(
                 [[[8.67275153907511, 45.681075508975425],
@@ -13,7 +16,4 @@ var AOI = /* color: #d63000 */ee.FeatureCollection(
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var int_find_city = require("users/emanuelespiritowork/SharedRepo:functions/int_find_city.js");
 
-var result = int_find_city.int_find_city({
-  AOI: AOI, 
-  start_date: "2023-11-11", 
-  end_date: "2023-12-11"});
+var result = int_find_city.int_find_city("2024-11-11","2024-11-13",AOI);
