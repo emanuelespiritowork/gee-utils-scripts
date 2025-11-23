4dc4d656fbc723878496b441755434bb6ecec3fc
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:16:48 +0000

    Updated functions/int_find_city_any.js (added 26 lines, deleted 21 lines).

--- functions/int_find_city_any.js
+++ functions/int_find_city_any.js
@@ -8,26 +8,31 @@ exports.int_find_city_any = function(img_coll, start_date, end_date, AOI, scale_
   scale_to_use = ee.Number(scale_to_use);
   threshold = ee.Number(threshold);
   
-  var filter = img_coll.filterDate(start_date, end_date);
+  var mosaic = mosaic_date.mosaic_date({
+    img_coll: img_coll,
+    AOI: AOI,
+    start_date: start_date,
+    latest_date: end_date,
+    scale_to_use: scale_to_use
+  });
   
-  var clip = clip_to.clip_to(filter,AOI,10).first();
-
-var vect = ee.Image(clip).select(["Gap_Filled_DNB_BRDF_Corrected_NTL"])
-.gt(ee.Image(40))
-.reduceToVectors({
-  bestEffort: true
-})
-.filter(ee.Filter.eq("label",1));
-
-Map.addLayer(clip, {
-  bands: "Gap_Filled_DNB_BRDF_Corrected_NTL",
-  min:10,
-  max:80
-});
-
-Map.addLayer(vect);
-
-Map.centerObject(AOI);
+  var vect = ee.Image(mosaic)
+  .select(["Gap_Filled_DNB_BRDF_Corrected_NTL"])
+  .gt(ee.Image(threshold))
+  .reduceToVectors({
+    bestEffort: true
+    })
+  .filter(ee.Filter.eq("label",1));
   
-  return(result);
+  Map.addLayer(clip, {
+    bands: "Gap_Filled_DNB_BRDF_Corrected_NTL",
+    min:40,
+    max:50
+  });
+  
+  Map.addLayer(vect);
+  
+  Map.centerObject(AOI);
+  
+  return(vect);
 };
