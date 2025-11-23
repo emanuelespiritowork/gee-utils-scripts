fddf8c6643771efa9836fc4eb6c2f67d3431d6f4
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-23 08:25:27 +0000

    Updated functions/int_find_city.js (added 6 lines, deleted 6 lines).

--- functions/int_find_city.js
+++ functions/int_find_city.js
@@ -13,12 +13,12 @@ exports.int_find_city = function(start_date, end_date, AOI, min_scale, min_value
   //print(end_date);
   
   var result = int_find_city_any.int_find_city_any({
-    img_coll: ee.ImageCollection("NASA/VIIRS/002/VNP46A2"),
-    start_date: start_date,
-    end_date: end_date,
-    AOI: AOI,
-    scale_to_use: scale_to_use,
-    threshold: threshold 
+    "img_coll": ee.ImageCollection("NASA/VIIRS/002/VNP46A2"),
+    "start_date": start_date,
+    "end_date": end_date,
+    "AOI": AOI,
+    "scale_to_use": scale_to_use,
+    "threshold": threshold 
   });
   
   return(result);
