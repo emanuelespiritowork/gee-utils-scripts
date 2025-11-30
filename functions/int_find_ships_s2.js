493580021a069fa10e7545ccb4a2c890ebd38d45
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-30 07:04:03 +0000

    Updated functions/int_find_ships_s2.js (added 9 lines, deleted 11 lines).

--- functions/int_find_ships_s2.js
+++ functions/int_find_ships_s2.js
@@ -1,4 +1,6 @@
 var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js");
+var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
+var s2_ndvi = require("users/emanuelespiritowork/SharedRepo:functions/s2_ndvi.js");
 
 exports.int_find_ships_s2 = function(img_coll,AOI,k_radius,n_iterations,min_scale){
   AOI = ee.FeatureCollection(AOI);
@@ -8,21 +10,17 @@ exports.int_find_ships_s2 = function(img_coll,AOI,k_radius,n_iterations,min_scal
   
   var clip = clip_to.clip_to(img_coll,AOI,scale_to_use);
   
-  //CONTINUE
-  
-  
-  var clip = clip_to.clip_to(s2_coll,AOI,scale_to_use);
-  
   var scale = s2_scale.s2_scale(clip);
   
   var s2_series = s2_ndvi.s2_ndvi(scale);
   
+  var mask_1 = s2_series.lt(-0.15);
   
-var mask_1 = img_coll.first().normalizedDifference(["B8","B4"]).lt(-0.15);
-var mask_2 = imageCollection.filterBounds(geometry).first().normalizedDifference(["B8","B4"]).gt(-0.4);
-var mask = mask_2.and(mask_1);
+  var mask_2 = s2_series.filterBounds(AOI).first().normalizedDifference(["B8","B4"]).gt(-0.4);
+  
+  var mask = mask_2.and(mask_1);
 
-var image = mask.clip(geometry);
+var image = mask.clip(AOI);
 // Define a kernel.
 var kernel = ee.Kernel.circle({radius: radius});
 
