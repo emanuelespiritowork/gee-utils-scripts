60b379127ff51677fd74d9bd192a265ba0003957
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-30 06:39:06 +0000

    Updated functions/int_find_city_any.js (added 6 lines, deleted 2 lines).

--- functions/int_find_city_any.js
+++ functions/int_find_city_any.js
@@ -9,11 +9,15 @@ exports.int_find_city_any = function(img_coll, start_date, end_date, AOI, scale_
   threshold = ee.Number(threshold);
   
   //al posto del mosaico metto il massimo nell'img_coll
-  var max = clip_to.clip_to(img_coll,AOI,scale_to_use).reduce({
+  var clip = clip_to.clip_to(img_coll,AOI,scale_to_use);
+  
+  Map.addLayer(clip.geometry(),{},"clip.geometry");
+  
+  var max = clip.reduce({
     reducer: ee.Reducer.max()
   });
   
-  Map.addLayer(max.geometry());
+  Map.addLayer(max.geometry(),{},"max.geometry");
   
   /*
   var mosaic = mosaic_date.mosaic_date(img_coll,
