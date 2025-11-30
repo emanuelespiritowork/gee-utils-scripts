f1be8ffaf7d96dcd55b150266b1fdebcef001124
emanuele.spirito.work <emanuele.spirito.work@gmail.com>
2025-11-30 07:36:04 +0000

    Updated functions/int_find_ships_s2.js (added 7 lines, deleted 6 lines).

--- functions/int_find_ships_s2.js
+++ functions/int_find_ships_s2.js
@@ -2,20 +2,21 @@ var clip_to = require("users/emanuelespiritowork/SharedRepo:functions/clip_to.js
 var s2_scale = require("users/emanuelespiritowork/SharedRepo:functions/s2_scale.js");
 var s2_ndvi = require("users/emanuelespiritowork/SharedRepo:functions/s2_ndvi.js");
 
-exports.int_find_ships_s2 = function(img_coll,AOI,max_value,min_value,k_radius,n_iterations,min_scale){
+exports.int_find_ships_s2 = function(img_coll,AOI,max_value,min_value,min_water,k_radius,n_iterations,min_scale){
   AOI = ee.FeatureCollection(AOI);
   var low_threshold = ee.Number(min_value || -0.4);
-  var high_threshold = ee.Number(max_value || -0.15);
-  var radius = k_radius || ee.Number(1);
-  var iterations = n_iterations || ee.Number(1);
-  var scale_to_use = min_scale || ee.Number(10);
+  var high_threshold = ee.Number(max_value || 0.1);
+  var water_threshold = ee.Number(min_water || 0.1);
+  var radius = ee.Number(k_radius || 1);
+  var iterations = ee.Number(n_iterations || 1);
+  var scale_to_use = ee.Number(min_scale || 10);
   
   var clip = clip_to.clip_to(img_coll,AOI,scale_to_use);
   
   var scale = s2_scale.s2_scale(clip);
   
   var find_ships = function(image){
-    var mask_water = image.select(["B8"]).gt(0.1);
+    var mask_water = image.select(["B8"]).gt(water_threshold);
     
     var ndvi = image.normalizedDifference(["B8","B4"]);
     
